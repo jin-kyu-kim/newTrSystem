f0f7b33e86726e8bc867dd02c5f83f96e3bc71cb
VtwHansolKim <157336316+VtwHansolKim@users.noreply.github.com>
2024-01-26 11:13:54 +0900

    테이블 생성 컴포넌트 수정: 버튼 조건에 따라 셀에 버튼 로직 추가

--- front/src/components/unit/CustomTable.js
+++ front/src/components/unit/CustomTable.js
@@ -1,19 +1,40 @@
 import DataGrid, { Column, Pager, Paging, } from "devextreme-react/data-grid";
+import { Button } from "devextreme-react/button";
 
-const CustomTable = ({ columns, values, onRowDblClick, pagerVisible  }) => {
+const CustomTable = ({ menuName, columns, values, onRowDblClick, pagerVisible  }) => {
 
   const gridRows = () => {
     const result = [];
     for(let i = 0; i < columns.length; i++) {
-        const { key, value, width, alignment } = columns[i];
-
-        result.push(
-            <Column key={key} dataField={key} caption={value} width={width} alignment={alignment || 'center'}>
+        const { key, value, width, alignment, button } = columns[i];
+        if(button) {
+          result.push(
+            <Column 
+              key={key} 
+              dataField={key} 
+              caption={value} 
+              width={width} 
+              alignment={alignment || 'center'}
+              cellRender={(e) => buttonRender(e)}>
             </Column>
         );
+        } else {
+          result.push(
+              <Column key={key} dataField={key} caption={value} width={width} alignment={alignment || 'center'}>
+              </Column>
+          );
+        }
     }
     return result;
   }
+
+  const buttonRender = (e) => {
+    if(menuName === "ProjectHrCtAprv") {
+      return(
+        <Button text="시간비용청구현황"/>
+      )
+    }
+	}
 
   return (
     <div className="wrap_table">
@@ -28,6 +49,12 @@ const CustomTable = ({ columns, values, onRowDblClick, pagerVisible  }) => {
         columnAutoWidth={false}
         noDataText=""
         onRowDblClick={onRowDblClick}
+        onCellPrepared={(e) => {
+          if (e.rowType === 'header') {
+            e.cellElement.style.textAlign = 'center';
+            e.cellElement.style.fontWeight = 'bold';
+          }
+        }}
     >
 
       <Paging defaultPageSize={20} />
