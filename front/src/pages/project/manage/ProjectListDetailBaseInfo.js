d570ac25a882d9b222ef7d866359d027eb1edecf
seulgi <seulgi.heo@vtw.co.kr>
2024-01-26 10:15:37 +0900

    1. 프로젝트 관리 목록<->상세 데이터 연동 및 상세기본정보js 구성  2.CustomHorizontalTable 최초생성

--- front/src/pages/project/manage/ProjectListDetailBaseInfo.js
+++ front/src/pages/project/manage/ProjectListDetailBaseInfo.js
@@ -1,31 +1,84 @@
 import React, { useEffect, useState } from 'react';
 
 import ApiRequest from '../../../utils/ApiRequest';
+import CustomHorizontalTable from '../../../components/unit/CustomHorizontalTable';
+import BaseInfo from '../manage/BaseInfo.json';
 
-const ProjectListDetailBaseInfo = () => {
-  const [tabData, setTabData] = useState([]);
+const ProjectListDetailBaseInfo = (data) => {
+  const [baseInfoData, setBaseInfoData] = useState([]);
+  const [picInfoData, setPicInfoData] = useState([]);
+  const [CnsrtmData, setCnsrtmData] = useState([]);
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await ApiRequest("api-endpoint-for-tab-1", param);
-//         setTabData(response.data);
-//       } catch (error) {
-//         console.error('Error fetching data for Tab 1:', error);
-//       }
-//     };
+  //baseInfoData데이터    ----> //prjctId가(백단에서 불러오는 데이터가 or 보내는 파라미터가) 동일하다면 동일한 useEffect 2건은 삭제처리 가능.
+  useEffect(() => {
+    const BaseInfoData = async () => {
+      const param = [ 
+        { tbNm: "PRJCT" }, 
+        { 
+         prjctId: data.data.projId, 
+        }, 
+     ]; 
+      try {
+        const response = await ApiRequest("/boot/common/commonSelect", param);
+        setBaseInfoData(response[0]);     
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+    BaseInfoData();
+  }, []);
 
-//     fetchData();
-//   }, []);
+  //picInfoData 데이터
+  useEffect(() => {
+    const param = [ 
+      { tbNm: "PRJCT" }, 
+      { 
+       prjctId: data.data.projId, 
+      }, 
+   ]; 
+    const PicInfoData = async () => {
+      try {
+        const response = await ApiRequest("/boot/common/commonSelect", param);
+        setPicInfoData(response[0]);  
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+    PicInfoData();
+  }, []);
+
+  //Cnsrtm 데이터 
+  useEffect(() => {
+    const param = [ 
+      { tbNm: "PRJCT" }, 
+      { 
+       prjctId: data.data.projId, 
+      }, 
+   ];  
+    const Cnsrtm = async () => {
+      try {
+        const response = await ApiRequest("/boot/common/commonSelect", param);
+        setCnsrtmData(response[0]);  
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+    Cnsrtm();
+  }, []);
+
 
   return (
-    <div>
-{/* 
-      {tabData.map((data, index) => (
-        <div key={index}>{data.someValue}</div>
-      ))}
-       */}
-       기본정보
+    <div style={{padding: '20px'}}>
+      <div className='container'>
+        <p><strong>* 기본정보</strong></p>
+        <CustomHorizontalTable headers={BaseInfo.BaseInfo} column={baseInfoData}/>
+        &nbsp;
+        <p><strong>* 담당자 정보</strong></p>
+        <CustomHorizontalTable headers={BaseInfo.PicInfo} column={picInfoData}/>
+        &nbsp;
+        <p><strong>* 컨소시엄</strong></p>
+        <CustomHorizontalTable headers={BaseInfo.Cnsrtm} column={CnsrtmData}/>
+      </div>
     </div>
   );
 };
