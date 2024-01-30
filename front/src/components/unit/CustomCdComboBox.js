c824a4985d2dc1b36c767bee50cefdece920ade2
VtwHansolKim <157336316+VtwHansolKim@users.noreply.github.com>
2024-01-30 19:56:20 +0900

    공통 컴포넌트 수정 및 추가: 기존 콤보박스 수정 및 새로운 콤보박스 컴포넌트 생성

---
+++ front/src/components/unit/CustomCdComboBox.js
@@ -0,0 +1,62 @@
+
+import React, { useEffect, useState, } from "react";
+
+import SelectBox from "devextreme-react/select-box";
+
+import cdJson from "./cd.json";
+import ApiRequest from "../../utils/ApiRequest";
+
+const CustomCdComboBox = ({ param, placeholderText, onSelect, name }) => {
+  const [cdVal, setCdVal] = useState([]);
+
+  useEffect(() => {
+    setCdVal(cdJson);
+    setCdVal((prevCdVal) => {
+      const updatedCdVal = [...prevCdVal];
+      updatedCdVal[1] = {
+        upCdValue: param,
+      };
+      return updatedCdVal;
+    });
+  }, []);
+
+  useEffect(() => {
+    if (cdVal.length === cdJson.length) {
+      getCode();
+    }
+  }, [cdVal]);
+
+  const getCode = async () => {
+    try {
+      const response = await ApiRequest("/boot/common/commonSelect", cdVal);
+
+      const updatedCdValues = response.map((item) => ({
+        cdValue: item.cdValue,
+        cdNm: item.cdNm,
+      }));
+
+      setCdVal(updatedCdValues);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    // <div className="dx-field-value">
+      <SelectBox
+        dataSource={cdVal}
+        displayExpr="cdNm"
+        valueExpr="cdValue"
+        placeholder={placeholderText}
+        onValueChanged={(e) => {
+          onSelect({ name, value: e.value });
+        }}
+        searchEnabled={true}
+        width="100%"
+        >
+      </SelectBox>
+    // </div>
+  );
+};
+
+export default React.memo(CustomCdComboBox);
