bd20ca61bf5ad671560d212839503786f47d15b1
ku1551 <jinkyu.kim@vtw.co.kr>
2024-02-05 17:58:02 +0900

    commit - "2024-02-05" 통계화면

--- front/src/pages/project/manage/ProjectTotCostInfo.js
+++ front/src/pages/project/manage/ProjectTotCostInfo.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import DataGrid, {
   Column,
-  Summary, 
+  Summary,
   GroupItem,
   Grouping,
   TotalItem,
@@ -13,21 +13,19 @@ import "devextreme/dist/css/dx.light.css";
 import "devextreme/dist/css/dx.material.blue.light.css";
 
 import ProjectTotCostInfoJson from "./ProjectTotCostInfoJson";
-import CustomTable from "components/unit/CustomTable";
 import ApiRequest from "../../../utils/ApiRequest";
 
-const ProjectTotCostInfo = (projIdInfo) => {
+const ProjectTotCostInfo = (projId) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     handelGetData();
-    console.log(data);
   }, []);
 
-  const handelGetData = () => {
+  const handelGetData = async () => {
     try {
-      ProjectTotCostInfoJson.params.map(async (item) => {
-        const modifiedItem = { ...item, prjctId: projIdInfo };
+      await ProjectTotCostInfoJson.params.map(async (item) => {
+        const modifiedItem = { ...item, prjctId: projId.projId };
         const response = await ApiRequest(
           "/boot/common/queryIdSearch",
           modifiedItem
@@ -39,10 +37,6 @@ const ProjectTotCostInfo = (projIdInfo) => {
     }
   };
 
-  const test = () => {
-    console.log("test");
-  };
-
   const gridRows = () => {
     const result = [];
     for (let i = 0; i < ProjectTotCostInfoJson.tableColumns.length; i++) {
@@ -52,8 +46,9 @@ const ProjectTotCostInfo = (projIdInfo) => {
           key={key}
           dataField={key}
           caption={value}
-          alignment="center"
-        ></Column>
+          alignment="right"
+          format={"#,##0"}
+        />
       );
     }
     return result;
@@ -61,16 +56,6 @@ const ProjectTotCostInfo = (projIdInfo) => {
 
   return (
     <div style={{ padding: "5%", height: "100%" }}>
-      <CustomTable
-        keyColumn={ProjectTotCostInfoJson.keyColumn}
-        columns={ProjectTotCostInfoJson.tableColumns}
-        values={data}
-        onRowDblClick={test}
-        paging={null}
-        summary={true}
-        summaryColumn={data}
-      />
-
       <DataGrid
         dataSource={data}
         keyExpr="id"
@@ -79,34 +64,64 @@ const ProjectTotCostInfo = (projIdInfo) => {
         masterDetail={{
           enabled: false,
         }}
+        onCellPrepared={(e) => {
+          if (e.rowType === "header") {
+            e.cellElement.style.textAlign = "center";
+            e.cellElement.style.fontWeight = "bold";
+          }
+        }}
       >
-        <Grouping allowCollapsing={false} texts="테스트" />
         {gridRows()}
+
+        <Column
+          dataField="bind"
+          caption="원가별집계"
+          customizeText={(e) => {
+            if (e.value === 1) {
+              return "인건비";
+            } else if (e.value === 2) {
+              return "경비";
+            } else {
+              return "재료비";
+            }
+          }}
+          groupIndex={0}
+        />
+
+        <Grouping autoExpandAll={true} />
 
         <Summary>
           <TotalItem
-            column="data"
+            column="costAmt"
             summaryType="sum"
             valueFormat="#,##0"
-            displayFormat="Total: {0}"
-            showInColumn="data"
+            displayFormat="{0}"
+          />
+          <TotalItem
+            column="useAmt"
+            summaryType="sum"
+            valueFormat="#,##0"
+            displayFormat="{0}"
+          />
+          <TotalItem
+            column="availAmt"
+            summaryType="sum"
+            valueFormat="#,##0"
+            displayFormat="{0}"
+          />
+          <TotalItem
+            column="useRate"
+            summaryType="avg"
+            valueFormat="#,##0"
+            displayFormat="{0}"
           />
         </Summary>
-        <Summary
-          totalItems={[
-            {
-              column: "data",
-              summaryType: "sum",
-              valueFormat: "#,##0",
-              displayFormat: "Total data: {0}",
-            },
-          ]}
-        />
         <GroupItem
-          column="ver"
+          column="bind"
           summaryType="sum"
           valueFormat="#,##0"
-          displayFormat={`Total data (version {0}): {1}`}
+          displayFormat="{0}"
+          showInGroupFooter={true}
         />
       </DataGrid>
     </div>
