ea798e46dc8d233dc917db314864260ec6e26a88
VtwHansolKim <157336316+VtwHansolKim@users.noreply.github.com>
2024-03-06 15:05:54 +0900

    validation 추가

--- front/src/components/unit/CustomDatePicker.js
+++ front/src/components/unit/CustomDatePicker.js
@@ -1,11 +1,21 @@
 import { DateBox } from "devextreme-react/date-box";
+import { Validator, RequiredRule } from "devextreme-react/validator";
 
 
-const CustomDatePicker = ({ onSelect, placeholder, value, name, readOnly }) => {
+const CustomDatePicker = ({ onSelect, label, placeholder, value, name, readOnly, required }) => {
     
     
+    const validate = () => {
+        if(required) {
+            return (
+                <RequiredRule message={`${label}은 필수 입력 값입니다.`}/>
+            )
+        }
+    }
+
     return (
         <DateBox
+            key={name}
             value={value}
             placeholder={placeholder}
             dateSerializationFormat="yyyyMMdd"
@@ -15,7 +25,9 @@ const CustomDatePicker = ({ onSelect, placeholder, value, name, readOnly }) => {
                 onSelect({name: name, value: e.value})
             }}
             readOnly={readOnly}
-        />
+        >
+            <Validator>{validate()}</Validator>
+        </DateBox>
     );
 }
 export default CustomDatePicker;
