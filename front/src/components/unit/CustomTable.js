1383f45a2f106dccf5844f1bdfbc1ef6cb8e6e20
VtwHansolKim <157336316+VtwHansolKim@users.noreply.github.com>
2024-03-08 11:02:24 +0900

    표출 페이지 선택을 총 페이지에 따라 다르게 보이게끔 수정, summary 소수점 지정 가능하게 수정

--- front/src/components/unit/CustomTable.js
+++ front/src/components/unit/CustomTable.js
@@ -3,7 +3,7 @@ import { Button } from "devextreme-react/button";
 import ToggleButton from "../../pages/sysMng/ToggleButton"
 
 const CustomTable = ({ keyColumn, pageSize, columns, values, onRowDblClick, paging, summary, summaryColumn, 
-                      handleYnVal, editRow, onEditRow, onClick,onRowClick }) => {
+                      handleYnVal, editRow, onEditRow, onClick, onRowClick }) => {
 
   const gridRows = () => {
     const result = [];
@@ -58,6 +58,22 @@ const CustomTable = ({ keyColumn, pageSize, columns, values, onRowDblClick, pagi
     )
   }
 
+  const allowedPageSize = () =>{
+    let pageSizes=[];
+    if(values.length < 20){
+      pageSizes = [5, 10, 'all']
+    }else if(values.length  < 50 ){
+      pageSizes =  [10, 20, 'all']
+    }else if(values.length  < 80) {
+      pageSizes =  [20, 50, 'all']
+    }else if(values.length  < 100){
+      pageSizes =  [20, 50, 80, 'all']
+    }else{
+      pageSizes =  [20, 50, 80, 100]
+    }
+    return pageSizes;
+  }
+
   return (
     <div className="wrap_table">
       <DataGrid
@@ -97,7 +113,7 @@ const CustomTable = ({ keyColumn, pageSize, columns, values, onRowDblClick, pagi
           showNavigationButtons={true}
           showInfo={false}
           showPageSizeSelector={true}
-          allowedPageSizes={[20, 50, 80, 100]}
+          allowedPageSizes={allowedPageSize()}
         />
         {gridRows()}
 
@@ -109,7 +125,7 @@ const CustomTable = ({ keyColumn, pageSize, columns, values, onRowDblClick, pagi
                 column={item.value}
                 summaryType={item.type}
                 displayFormat={item.format}
-                valueFormat={{ type: 'fixedPoint', precision: 0 }} // 천 단위 구분자 설정
+                valueFormat={{ type: 'fixedPoint', precision: item.precision }} // 천 단위 구분자 설정
               />
             ))}
           </Summary>
