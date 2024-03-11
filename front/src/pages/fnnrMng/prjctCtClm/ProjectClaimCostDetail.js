90413b9cd312ce51a447122ffaccd65eb6b6cc6f
soo-yeon-yoon <sooyeon.yoon@vtw.co.kr>
2024-03-11 20:35:21 +0900

    프로젝트비용청구현황 일자별 조회 엑셀 다운로드, 검색 적용

--- front/src/pages/fnnrMng/prjctCtClm/ProjectClaimCostDetail.js
+++ front/src/pages/fnnrMng/prjctCtClm/ProjectClaimCostDetail.js
@@ -1,48 +1,58 @@
 import React, { useCallback, useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
-import { TabPanel } from "devextreme-react";
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from 'react-router-dom';
+import {Button, TabPanel} from "devextreme-react";
 import ApiRequest from "../../../utils/ApiRequest";
 
 import ProjectClaimCostDetailJson from "./ProjectClaimCostDetailJson.json";
 import SearchPrjctCostSet from "../../../components/composite/SearchPrjctCostSet";
 
 const ProjectCostClaimDetail = () => {
-  const location = useLocation();
-  const prjctId = location.state.prjctId;
-  const bgtMngOdr = location.state.bgtMngOdr;
-  const ctrtYmd = location.state.ctrtYmd;
-  const bizEndYmd = location.state.bizEndYmd;
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [atrzLnSn, setAtrzLnSn] = useState();
+    const location = useLocation();
+    const navigate = useNavigate();
+    const prjctId = location.state.prjctId;
+    const prjctNm = location.state.prjctNm;
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const ProjectClaimCostDetail = ProjectClaimCostDetailJson.tabMenu;
-  const searchParams = ProjectClaimCostDetailJson.searchParams;
-  const [param, setParam] = useState([]);
+    const ProjectClaimCostDetail = ProjectClaimCostDetailJson.tabMenu;
+    const searchParams = ProjectClaimCostDetailJson.searchParams;
+    const [param, setParam] = useState([]);
 
-  useEffect(() => {
-  
-    const param = { 
-      queryId: "projectMapper.retrievePrjctAtrzLnSn",
-      prjctId: prjctId,
-    };
+    let year = "";
+    let monthVal = "";
+    let aplyOdr = 0;
+    let empId = "";
 
-    const response = ApiRequest("/boot/common/queryIdSearch", param);
+    useEffect(() => {
 
-    response.then((value) => {
-      if(value[0] !== null) {
-        setAtrzLnSn(value[0].atrzLnSn);
-      }
-    });
-  
-  }, []);
+        const param = {
+            queryId: "financialAffairMngMapper.retrievePrjctCtClmSttusYMDAccto",
+            prjctId: prjctId,
+            year: year,
+            monthVal: monthVal,
+            aplyOdr: aplyOdr,
+            empId: empId,
+        };
+
+        const response = ApiRequest("/boot/common/queryIdSearch", param);
+
+    }, []);
 
     const searchHandle = async (initParam) => {
         if(initParam.year == null || initParam.month == null) {
+
+            const date = new Date();
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+
+            let odrVal = day > 15 ? "2" : "1";
+            let monthVal = month < 10 ? "0" + month : month;
+
             setParam({
                 ...param,
-                aplyNm: initParam.year + initParam.month,
-                aplyOdr: initParam.aplyOdr,
+                year: year,
+                monthVal: monthVal,
+                aplyOdr: odrVal,
                 empId: initParam.empId,
             })
 
@@ -50,67 +60,84 @@ const ProjectCostClaimDetail = () => {
         };
 
         setParam({
+
             ...param,
-            aplyNm: initParam.year + initParam.month,
+            year: initParam.year,
+            monthVal: initParam.month,
             aplyOdr: initParam.aplyOdr,
             empId: initParam.empId,
         })
     }
 
-  // 탭 변경시 인덱스 설정
-  const onSelectionChanged = useCallback(
-    (args) => {
-      if (args.name === "selectedIndex") {
-        setSelectedIndex(args.value);
-      }
-    },
-    [setSelectedIndex]
-  );
-  
-  const itemTitleRender = (a) => <span>{a.TabName}</span>;
-  
-  return (
-    <div>
-      <div
-        className="title p-1"
-        style={{ marginTop: "20px", marginBottom: "10px" }}
-      >
-          <div style={{marginRight: "20px", marginLeft: "20px"}}>
-              <h1 style={{fontSize: "30px"}}>프로젝트비용청구현황</h1>
-              <div>{location.state.prjctNm}</div>
-              <div className="wrap_search" style={{marginBottom: "20px"}}>
-                  <SearchPrjctCostSet callBack={searchHandle} props={searchParams}/>
-              </div>
-          </div>
-      </div>
-        <div
-            style={{
-                marginTop: "20px",
-                marginBottom: "10px",
-                width: "100%",
-                height: "100%",
-            }}
-        >
-        <TabPanel
-          height="auto"
-          width="auto"
-          dataSource={ProjectClaimCostDetail}
-          selectedIndex={selectedIndex}
-          onOptionChanged={onSelectionChanged}
-          itemTitleRender={itemTitleRender}
-          animationEnabled={true}
-          itemComponent={({ data }) => {
-          const Component = React.lazy(() => import(`${data.url}`));
-          return (
-            <React.Suspense fallback={<div>Loading...</div>}>
-              <Component prjctId={prjctId} ctrtYmd={ctrtYmd} bizEndYmd={bizEndYmd} bgtMngOdr={bgtMngOdr} />
-            </React.Suspense>
-          );
-        }}
-        />
-      </div>
-    </div>
-  );
+    // 탭 변경시 인덱스 설정
+    const onSelectionChanged = useCallback(
+        (args) => {
+            if (args.name === "selectedIndex") {
+                setSelectedIndex(args.value);
+            }
+        },
+        [setSelectedIndex]
+    );
+
+    const itemTitleRender = (a) => <span>{a.TabName}</span>;
+
+
+    return (
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+            <div className="container" >
+                <div className="col-md-10 mx-auto" style={{ marginTop: "20px", marginBottom: "10px" }}>
+                    <h1 style={{fontSize: "30px"}}>프로젝트비용청구현황</h1>
+                </div>
+                <div className="col-md-10 mx-auto" style={{ marginBottom: "10px" }}>
+                    <span>* {prjctNm}({param.year}{param.monthVal}-{param.aplyOdr})</span>
+                </div>
+                <div className="wrap_search" style={{marginBottom: "20px"}}>
+                    <SearchPrjctCostSet callBack={searchHandle} props={searchParams} />
+                </div>
+
+
+                <div
+                    style={{
+                        marginTop: "20px",
+                        marginBottom: "10px",
+                        width: "100%",
+                        height: "100%",
+                    }}
+                >
+                    <TabPanel
+                        height="auto"
+                        width="auto"
+                        dataSource={ProjectClaimCostDetail}
+                        selectedIndex={selectedIndex}
+                        onOptionChanged={onSelectionChanged}
+                        itemTitleRender={itemTitleRender}
+                        animationEnabled={true}
+                        itemComponent={({ data }) => { console.log(param);
+                            const Component = React.lazy(() => import(`${data.url}`));
+                            return (
+                                <React.Suspense fallback={<div>Loading...</div>}>
+                                    <Component prjctId={prjctId}
+                                               prjctNm={prjctNm}
+                                               year={param.year}
+                                               monthVal={param.monthVal}
+                                               aplyOdr={param.aplyOdr}
+                                               empId={param.empId} />
+                                </React.Suspense>
+                            );
+                        }}
+                    />
+                </div>
+
+            </div>
+            <Button
+                id="button"
+                text="목록"
+                className="btn_submit filled_gray"
+                style={{ alignSelf: "center" }}
+                onClick={() => navigate("../fnnrMng/prjctCtClm/ProjectClaimCost")}
+            />
+        </div>
+    );
 };
 
 export default ProjectCostClaimDetail;
--- front/src/pages/fnnrMng/prjctCtClm/ProjectClaimCostYMD.js
+++ front/src/pages/fnnrMng/prjctCtClm/ProjectClaimCostYMD.js
@@ -1,31 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import DataGrid, { Column } from 'devextreme-react/data-grid';
+import DataGrid, {Column, Export} from 'devextreme-react/data-grid';
 import ProjectClaimCostYMDJson from '../prjctCtClm/ProjectClaimCostYMDJson.json';
 import ApiRequest from "../../../utils/ApiRequest";
+import {Workbook} from "exceljs";
+import {exportDataGrid} from "devextreme/excel_exporter";
+import { saveAs } from 'file-saver';
+import '../../../assets/css/Style.css'
 
 
-const ProjectClaimCostYMD = () => {
-  const { keyColumn, queryId } = ProjectClaimCostYMDJson;
-  const [empMMData, setEmpMMData] = useState([]);
-  const [empCostData, setEmpCostData] = useState([]);
+const ProjectClaimCostYMD = ({ prjctId, prjctNm, year, monthVal, aplyOdr, empId }) => {
+    const { keyColumn, queryId } = ProjectClaimCostYMDJson;
+    const [empMMData, setEmpMMData] = useState([]);
+    const [empCostData, setEmpCostData] = useState([]);
 
     // 날짜 출력 함수
-    const generateDates = (year, month, odr) => {
-        const searchYear = year || new Date().getFullYear();
-        const searchMonth = month || new Date().getMonth() + 1;
+    const generateDates = (year, monthVal, aplyOdr) => {
+
         const formattedDates = [];
 
-        if(odr == 1){
+        if(aplyOdr == 1){
             for (let day = 1; day <= 15; day++) {
                 const formattedDay = day < 10 ? `0${day}` : `${day}`;
-                const formattedDate = `${searchYear}${searchMonth}${formattedDay}`;
+                const formattedDate = `${year}${monthVal}${formattedDay}`;
                 formattedDates.push(formattedDate);
             }
-        } else if (odr == 2){
-            const lastDay = new Date(searchYear, searchMonth, 0).getDate();
+        } else if (aplyOdr == 2){
+            const lastDay =  new Date(year, monthVal, 0).getDate();
             for (let day = 16; day <= lastDay; day++) {
                 const formattedDay = `${day}`;
-                const formattedDate = `${searchYear}${searchMonth}${formattedDay}`;
+                const formattedDate = `${year}${monthVal}${formattedDay}`;
                 formattedDates.push(formattedDate);
             }
         }
@@ -37,13 +40,17 @@ const ProjectClaimCostYMD = () => {
     useEffect(() => {
         getEmpMMData();
         getEmpCostData();
-    }, []);
+    }, [year, monthVal, aplyOdr, empId]);
 
     // 수행인력 컬럼 데이터 조회
     const getEmpMMData = async () => {
         const param = {
             queryId: queryId.empMMQueryId,
-            prjctId: keyColumn.value
+            prjctId: keyColumn.value,
+            aplyYm: year+monthVal,
+            aplyOdr: aplyOdr,
+            empId: empId
+
         };
         try{
             const response = await ApiRequest('/boot/common/queryIdSearch', param);
@@ -57,7 +64,10 @@ const ProjectClaimCostYMD = () => {
     const getEmpCostData = async () => {
         const param = {
             queryId: queryId.empCostQueryId,
-            prjctId: keyColumn.value
+            prjctId: keyColumn.value,
+            aplyYm: year+monthVal,
+            aplyOdr: aplyOdr,
+            empId: empId
         };
         try{
             const response = await ApiRequest('/boot/common/queryIdSearch', param);
@@ -69,12 +79,12 @@ const ProjectClaimCostYMD = () => {
 
     // 날짜 컬럼 데이터 렌더링
     const claimDateCellRender = (data) => {
-        const generatedDates = generateDates('2024', '03', 1);
+        const generatedDates = generateDates(year, monthVal, aplyOdr);
         const currentDate = generatedDates[data.rowIndex];
 
         return (
             <div style={{ display: "flex" }}>
-                    <div>{generatedDates[data.rowIndex]}</div>
+                <div>{generatedDates[data.rowIndex]}</div>
             </div>
         );
     };
@@ -121,16 +131,85 @@ const ProjectClaimCostYMD = () => {
         )
     }
 
+    const getEmpMMDataForDate = (date) => {
+        const filteredEmpMMData = empMMData.filter((m) => m.aplyYmd === date);
+        return filteredEmpMMData.map((m) => `${m.empFlnm} / ${m.md} / ${m.mmAtrzCmptnYn}`).join(', ');
+    };
 
-    const rowData = generateDates('2024', '03', 1 ).map((date) => ({ cliamDate: date }));
+    const getEmpCostDataForDate = (date) => {
+        const filteredEmpCostData = empCostData.filter((m) => m.utztnDt === date);
+        return filteredEmpCostData.map((m) => `${m.empFlnm} / ${m.expensCd} / ${m.utztnAmt} / (${m.useOffic}) / 상세내역 : ${m.cdPrpos} / 목적 : ${m.atdrn} / ${m.ctAtrzCmptnYn}`).join(', ');
+    };
+
+    const rowData = generateDates(year, monthVal, aplyOdr ).map((date) => ({
+        cliamDate: date,
+        empMM: getEmpMMDataForDate(date),
+        empCost: getEmpCostDataForDate(date)
+    }));
+
+    // 엑셀 출력
+    const onExporting = (e) => {
+        const currentDateTime = new Date();
+        const formattedDateTime = `${currentDateTime.getFullYear()}`+
+            `${padNumber(currentDateTime.getMonth() + 1)}`+
+            `${padNumber(currentDateTime.getDate())}`+
+            `${padNumber(currentDateTime.getHours())}`+
+            `${padNumber(currentDateTime.getMinutes())}`+
+            `${padNumber(currentDateTime.getSeconds())}`;
+        if (e.component) {
+            const workbook = new Workbook();
+            const worksheet = workbook.addWorksheet(`${prjctNm}.${year}${monthVal}-${aplyOdr}_${formattedDateTime.substring(0, 6)}`);
+            exportDataGrid({
+                component: e.component,
+                worksheet,
+                autoFilterEnabled: true,
+            }).then(() => {
+                workbook.xlsx.writeBuffer().then((buffer) => {
+                    saveAs(new Blob([buffer],
+                            { type: 'application/octet-stream' }),
+                        `${prjctNm}.${year}${monthVal}-${aplyOdr}_${formattedDateTime}.xlsx`);
+                });
+            });
+        }
+    };
+
+    // 숫자를 두 자릿수로 만들어주는 함수
+    const padNumber = (num) => {
+        return num.toString().padStart(2, '0');
+    };
 
     return (
         <div style={{padding: '20px'}}>
             <div className='container'>
-                <DataGrid dataSource={rowData} showBorders={true}>
-                    <Column dataField="cliamDate" caption="날짜" cellRender={(data) => claimDateCellRender(data)}/>
-                    <Column dataField="empMM" caption="수행인력" cellRender={(data) => empMMCellRender(data)}/>
-                    <Column dataField="empCost" caption="경비" cellRender={(data) => empCostCellRender(data)}/>
+                <DataGrid
+                    dataSource={rowData}
+                    showBorders={true}
+                    showColumnLines={true}
+                    onExporting={onExporting}
+                    onCellPrepared={(e) => {
+                        if (e.rowType === 'header') {
+                            e.cellElement.style.textAlign = 'center';
+                            e.cellElement.style.fontWeight = 'bold';
+                        }
+                    }}
+                >
+                    <Column
+                        dataField="cliamDate"
+                        caption="날짜"
+                        cellRender={(data) => claimDateCellRender(data)}
+                        width="150"
+                    />
+                    <Column
+                        dataField="empMM"
+                        caption="수행인력"
+                        cellRender={(data) => empMMCellRender(data)}
+                        width="250"
+                    />
+                    <Column
+                        dataField="empCost"
+                        caption="경비"
+                        cellRender={(data) => empCostCellRender(data)}/>
+                    <Export enabled={true} />
                 </DataGrid>
             </div>
         </div>
