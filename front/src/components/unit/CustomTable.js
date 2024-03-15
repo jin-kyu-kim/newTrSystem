0f796a87ea2fd4ea67445161af7b2becfc270b1d
VtwHansolKim <157336316+VtwHansolKim@users.noreply.github.com>
2024-03-15 18:18:33 +0900

    customTable 버튼 구분될 수 있도록 수정

--- front/src/components/unit/CustomTable.js
+++ front/src/components/unit/CustomTable.js
@@ -53,23 +53,25 @@ const CustomTable = ({ keyColumn, pageSize, columns, values, onRowDblClick, pagi
 
   const buttonRender = (button, data) => {
     return(
-      <Button text={button} onClick={() => {onClick(data)}}/>
+      <Button name={button.name} text={button.text} onClick={(e) => {onClick(button, data)}}/>
       
     )
   }
 
   const allowedPageSize = () =>{
     let pageSizes=[];
-    if(values.length < 20){
-      pageSizes = [5, 10, 'all']
-    }else if(values.length  < 50 ){
-      pageSizes =  [10, 20, 'all']
-    }else if(values.length  < 80) {
-      pageSizes =  [20, 50, 'all']
-    }else if(values.length  < 100){
-      pageSizes =  [20, 50, 80, 'all']
-    }else{
-      pageSizes =  [20, 50, 80, 100]
+    if(values != null) {
+      if(values.length < 20){
+        pageSizes = [5, 10, 'all']
+      }else if(values.length  < 50 ){
+        pageSizes =  [10, 20, 'all']
+      }else if(values.length  < 80) {
+        pageSizes =  [20, 50, 'all']
+      }else if(values.length  < 100){
+        pageSizes =  [20, 50, 80, 'all']
+      }else{
+        pageSizes =  [20, 50, 80, 100]
+      }
     }
     return pageSizes;
   }
