c1fa73c6cd35db34318848ce509f04f2c38b6c35
soo-yeon-yoon <sooyeon.yoon@vtw.co.kr>
2024-03-19 20:44:42 +0900

    경비 승인내역 비용코드별, 프로젝트별 소계 및 검색 구현

--- front/src/pages/fnnrMng/EmpExpenseAprvList.js
+++ front/src/pages/fnnrMng/EmpExpenseAprvList.js
@@ -1,16 +1,12 @@
 import React, { useCallback, useState, useEffect } from "react";
-import DataGrid, {Column, Export} from 'devextreme-react/data-grid';
-import { DateBox } from 'devextreme-react';
-import { useNavigate, useLocation } from 'react-router-dom';
 import {Button, TabPanel} from "devextreme-react";
 import ApiRequest from "../../utils/ApiRequest";
 
+import SearchOdrRange from "../../components/composite/SearchOdrRange";
 import EmpExpenseAprvListJson from "./EmpExpenseAprvListJson.json";
-import SearchPrjctCostSet from "../../components/composite/SearchPrjctCostSet";
 
 const EmpExpenseAprvList = () => {
 
-  const navigate = useNavigate();
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   const EmpExpenseAprvList = EmpExpenseAprvListJson.tabMenu;
@@ -20,15 +16,15 @@ const EmpExpenseAprvList = () => {
   let year = "";
   let monthVal = "";
   let aplyOdr = 0;
-  let empId = "";
 
   useEffect(() => {
 
     const param = {
-      queryId: "financialAffairMngMapper.retrievePrjctCtClmSttusYMDMMAccto",
-      year: year,
-      monthVal: monthVal,
-      aplyOdr: aplyOdr,
+      queryId: "financialAffairMngMapper.retrieveExpensAprvDtlsPrjctAccto",
+      startYm: year+monthVal,
+      startOdr: aplyOdr,
+      endYm: year+monthVal,
+      endOdr: aplyOdr,
     };
 
     const response = ApiRequest("/boot/common/queryIdSearch", param);
@@ -36,6 +32,7 @@ const EmpExpenseAprvList = () => {
   }, []);
 
   const searchHandle = async (initParam) => {
+      console.log('iii',initParam);
     if(initParam.year == null || initParam.month == null) {
 
       const date = new Date();
@@ -43,14 +40,15 @@ const EmpExpenseAprvList = () => {
       const month = date.getMonth() + 1;
       const day = date.getDate();
 
-      let odrVal = day > 15 ? "2" : "1";
+      let odrVal = day > 15 ? "1" : "2";
       let monthVal = month < 10 ? "0" + month : month;
 
       setParam({
         ...param,
-        year: year,
-        monthVal: monthVal,
-        aplyOdr: odrVal,
+          startYm: year+monthVal,
+          startOdr: odrVal,
+          endYm: year+monthVal,
+          endOdr: odrVal,
       })
 
       return;
@@ -59,9 +57,10 @@ const EmpExpenseAprvList = () => {
     setParam({
 
       ...param,
-      year: initParam.year,
-      monthVal: initParam.month,
-      aplyOdr: initParam.aplyOdr,
+        startYm: initParam.startYm,
+        startOdr: initParam.startOdr,
+        endYm: initParam.endYm,
+        endOdr: initParam.endOdr,
     })
   }
 
@@ -79,42 +78,47 @@ const EmpExpenseAprvList = () => {
 
   return (
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-        <div className="container">
-          <div className="col-md-10 mx-auto" style={{marginTop: "20px", marginBottom: "10px"}}>
-            <h1 style={{fontSize: "30px"}}>경비 승인내역</h1>
+          <div className="container">
+              <div className="col-md-10 mx-auto" style={{marginTop: "20px", marginBottom: "10px"}}>
+                  <h1 style={{fontSize: "30px"}}>경비 승인내역</h1>
+              </div>
+              <div className="col-md-10 mx-auto" style={{marginBottom: "10px"}}>
+                  <span>* 일자 선택 시 해당 일자가 속한 차수 기준 검색</span>
+              </div>
+              <div className="wrap_search" style={{marginBottom: "20px"}}>
+                  {/*<SearchPrjctCostSet callBack={searchHandle} props={searchParams}/>*/}
+                  <SearchOdrRange callBack={searchHandle} props={searchParams}/>
+              </div>
+              <div
+                  style={{
+                      marginTop: "20px",
+                      marginBottom: "10px",
+                      width: "100%",
+                      height: "100%",
+                  }}
+              >
+                  <TabPanel
+                      height="auto"
+                      width="auto"
+                      dataSource={EmpExpenseAprvList}
+                      selectedIndex={selectedIndex}
+                      onOptionChanged={onSelectionChanged}
+                      itemTitleRender={itemTitleRender}
+                      animationEnabled={true}
+                      itemComponent={({data}) => {
+                          const Component = React.lazy(() => import(`${data.url}`));
+                          return (
+                              <React.Suspense fallback={<div>Loading...</div>}>
+                                  <Component startYm={param.startYm}
+                                             startOdr={param.startOdr}
+                                             endYm={param.endYm}
+                                             endOdr={param.endOdr} />
+                              </React.Suspense>
+                          );
+                      }}
+                  />
+              </div>
           </div>
-          <div className="wrap_search" style={{marginBottom: "20px"}}>
-            <SearchPrjctCostSet callBack={searchHandle} props={searchParams}/>
-          </div>
-          <div
-              style={{
-                marginTop: "20px",
-                marginBottom: "10px",
-                width: "100%",
-                height: "100%",
-              }}
-          >
-            <TabPanel
-                height="auto"
-                width="auto"
-                dataSource={EmpExpenseAprvList}
-                selectedIndex={selectedIndex}
-                onOptionChanged={onSelectionChanged}
-                itemTitleRender={itemTitleRender}
-                animationEnabled={true}
-                itemComponent={({data}) => {
-                  const Component = React.lazy(() => import(`${data.url}`));
-                  return (
-                      <React.Suspense fallback={<div>Loading...</div>}>
-                        <Component year={param.year}
-                                   monthVal={param.monthVal}
-                                   aplyOdr={param.aplyOdr}/>
-                      </React.Suspense>
-                  );
-                }}
-            />
-          </div>
-        </div>
       </div>
   );
 
