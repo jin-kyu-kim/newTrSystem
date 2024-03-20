7dd4c8b3eee9b299b5c7a6fb9530f33a5d91e8f3
soo-yeon-yoon <sooyeon.yoon@vtw.co.kr>
2024-03-20 19:05:04 +0900

    경비 승인내역 조회 조건 변경, 검색 기준대로 날짜 컬럼 렌더링

--- front/src/components/composite/SearchOdrRange.js
+++ front/src/components/composite/SearchOdrRange.js
@@ -4,10 +4,41 @@ import { Box, Item } from "devextreme-react/box";
 import CustomDateRangeBox from "../unit/CustomDateRangeBox";
 import AutoCompleteProject from "../unit/AutoCompleteProject";
 import AutoCompleteName from "../unit/AutoCompleteName";
+import {SelectBox} from "devextreme-react/select-box";
 
 const SearchOdrRange = ({ callBack, props }) => {
 
     const [initParam, setInitParam] = useState([]);
+    const [yearData, setYearData] = useState([]);
+    const [monthData, setMonthData] = useState([]);
+    const [aplyOdrVisible, setAplyOdrVisible] = useState(true);
+
+    const yearList = [];
+    const monthList = [];
+
+    const inqMthd = [
+        {
+            "value": "odr",
+            "text": "차수별"
+        },
+        {
+            "value": "month",
+            "text": "월별"
+        }
+    ];
+
+    const odrList = [
+        {
+            "id": "1",
+            "value": "1",
+            "text": "1회차"
+        },
+        {
+            "id": "2",
+            "value": "2",
+            "text": "2회차"
+        }
+    ];
 
     useEffect(() => {
         const date = new Date();
@@ -15,17 +46,33 @@ const SearchOdrRange = ({ callBack, props }) => {
         const month = date.getMonth() + 1;
         const day = date.getDate();
 
-        let odrVal = day > 15 ? "2" : "1";
+        const startYear = year - 10;
+        const EndYear = year + 1;
+
+
+        for(let i = startYear; i <= EndYear; i++) {
+            yearList.push({"value": i});
+        }
+
+        for(let i = 1; i <= 12; i++) {
+            if(i < 10) {
+                i = "0" + i;
+            }
+            monthList.push({"value": i});
+        }
+
+        let odrVal = day > 15 ? "1" : "2";
         let monthVal = month < 10 ? "0" + month : month;
 
         setInitParam({
-            startYmd: year+monthVal,
-            startOdr: odrVal,
-            endYmd: year+monthVal,
-            endOdr: odrVal,
-            prjctId: "",
-            empId: "",
+            inqMthd : "odr",
+            yearItem: year,
+            monthItem: monthVal,
+            aplyOdr: odrVal
         });
+
+        setYearData(yearList);
+        setMonthData(monthList);
 
         callBack(initParam);
     }, []);
@@ -37,8 +84,7 @@ const SearchOdrRange = ({ callBack, props }) => {
         // 시작일자가 변경될 때 수행할 로직 추가
         setInitParam({
             ...initParam,
-            startYmd: startYmd,
-            startOdr: startOdr
+            startYmOdr: startYmd+startOdr
         });
     };
 
@@ -49,8 +95,7 @@ const SearchOdrRange = ({ callBack, props }) => {
         // 종료일자가 변경될 때 수행할 로직 추가
         setInitParam({
             ...initParam,
-            endYmd: endYmd,
-            endOdr: endOdr
+            endYmOdr: endYmd+endOdr
         });
     };
 
@@ -68,6 +113,24 @@ const SearchOdrRange = ({ callBack, props }) => {
         });
     };
 
+    const handleChgState = ({ name, value }) => {
+        setInitParam({
+            ...initParam,
+            [name]: value
+        })
+
+    }
+
+    // 조회방식이 월별인 경우 차수 selectbox를 숨김
+    const handleInqMthd = (value) => {
+
+        if (value === "month") {
+            setAplyOdrVisible(false);
+        } else {
+            setAplyOdrVisible(true);
+        }
+    }
+
     const handleSubmit = () => {
         callBack(initParam);
     }
@@ -75,12 +138,69 @@ const SearchOdrRange = ({ callBack, props }) => {
     return (
         <div className="box_search" width="100%">
             <Box direction="row" width={"100%"} height={50} >
-                <Item className="prjctDatePickerItem" ratio={2} visible={true}>
-                    <CustomDateRangeBox
-                        onStartDateChange={handleStartDateChange}
-                        onEndDateChange={handleEndDateChange}
+                <Item ratio={0} baseSize={"120"}>
+                    <SelectBox
+                        dataSource={inqMthd}
+                        name="inqMthd"
+                        displayExpr={"text"}
+                        valueExpr={"value"}
+                        onValueChanged={(e) => {
+                            handleInqMthd(e.value);
+                            handleChgState({name: e.component.option("name"), value: e.value });
+                        }}
+                        placeholder="[조회방식]"
+                        style={{margin: "0px 5px 0px 5px"}}
+                        value={initParam.inqMthd}
                     />
                 </Item>
+                { props.yearItem &&
+                    <Item ratio={0} baseSize={"120"}>
+                        <SelectBox
+                            dataSource={yearData}
+                            name="yearItem"
+                            displayExpr={"value"}
+                            valueExpr={"value"}
+                            onValueChanged={(e) => handleChgState({name: e.component.option("name"), value: e.value })}
+                            placeholder="[연도]"
+                            style={{margin: "0px 5px 0px 5px"}}
+                            value={initParam.yearItem}
+                        />
+                    </Item>
+                }
+                { props.monthItem &&
+                    <Item ratio={0} baseSize={"120"}>
+                        <SelectBox
+                            dataSource={monthData}
+                            name="monthItem"
+                            displayExpr={"value"}
+                            valueExpr={"value"}
+                            onValueChanged={(e) => handleChgState({name: e.component.option("name"), value: e.value })}
+                            placeholder="[월]"
+                            style={{margin: "0px 5px 0px 5px"}}
+                            value={initParam.monthItem}
+                        />
+                    </Item>
+                }
+                { props.aplyOdr && aplyOdrVisible &&
+                    <Item ratio={0} baseSize={"120"}>
+                        <SelectBox
+                            dataSource={odrList}
+                            name="aplyOdr"
+                            displayExpr={"text"}
+                            valueExpr={"value"}
+                            onValueChanged={(e) => handleChgState({name: e.component.option("name"), value: e.value })}
+                            placeholder="[차수]"
+                            style={{margin: "0px 5px 0px 5px"}}
+                            value={initParam.aplyOdr}
+                        />
+                    </Item>
+                }
+                {/*<Item className="prjctDatePickerItem" ratio={2} visible={true}>*/}
+                {/*    <CustomDateRangeBox*/}
+                {/*        onStartDateChange={handleStartDateChange}*/}
+                {/*        onEndDateChange={handleEndDateChange}*/}
+                {/*    />*/}
+                {/*</Item>*/}
                 {/*<Item className="prjctNameItem" ratio={1} visible={props.prjctNameItem}>*/}
                 {/*<Item className="prjctNameItem" ratio={1} visible={true}>*/}
                 {/*    <AutoCompleteProject*/}
