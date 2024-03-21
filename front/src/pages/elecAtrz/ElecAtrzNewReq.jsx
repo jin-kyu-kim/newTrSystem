02ab0fbf310fadc62163c715116c86a68a86f1d2
VtwHansolKim <157336316+VtwHansolKim@users.noreply.github.com>
2024-03-21 17:31:44 +0900

    전자결재 서식 조회 수정중

--- front/src/pages/elecAtrz/ElecAtrzNewReq.jsx
+++ front/src/pages/elecAtrz/ElecAtrzNewReq.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import { Button } from "devextreme-react/button"; 
+import { TextBox } from "devextreme-react/text-box";
+import { FileUploader } from "devextreme-react/file-uploader";
 import HtmlEditBox from "components/unit/HtmlEditBox";
+import ApiRequest from "utils/ApiRequest";
+
+import ElecAtrzOutordEmpCtrt from "./ElecAtrzOutordEmpCtrt";
 
 const ElecAtrzNewReq = () => {
 
     const location = useLocation();
     const prjctId = location.state.prjctId;
-    
+    const formData = location.state.formData;
+    const [prjctData, setPrjctData] = useState({});
+    const [data, setData] = useState([]);
+
     const navigate = useNavigate();
+
+    const column = { "dataField": "gnrlAtrzCn", "placeholder": "내용을 입력해주세요."};
+
+    useEffect(() => {
+        console.log(formData)
+        
+        retrievePrjctInfo();
+
+        setData(formData);
+    }, []);
+
+    const retrievePrjctInfo = async () => {
+        const param = [
+            { tbNm: "PRJCT" },
+            { prjctId: prjctId}
+        ]
+        try {
+            const response = await ApiRequest("/boot/common/commonSelect", param);
+            setPrjctData(response[0]);
+        } catch (error) {
+            console.error(error)
+        }
+    };
 
     /**
      * 결재요청 버튼 클릭시 전자결재 요청 함수 실행
@@ -39,8 +70,6 @@ const ElecAtrzNewReq = () => {
         navigate("../elecAtrz/elecAtrzForm", {state: {prjctId: prjctId}});
     }
 
-    const test = "<p><span style=\"color: rgb(68, 68, 68); background-color: rgb(255, 255, 255); font-size: 14px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">1. 프로젝트 투입 인력의 인사 관련 처리 지원 및 인턴 수배 등의 요청에 대한 품의 양식입니다.</span></p><p><span style=\"color: rgb(68, 68, 68); background-color: rgb(255, 255, 255); font-size: 14px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">2. 결재선은 PM 또는 팀리더 - [심사자 : 팀장 &gt; 승인자 : 대표이사], 팀장 - [승인자 : 대표이사] 로 지정합니다.</span></p><p><span style=\"color: rgb(68, 68, 68); background-color: rgb(255, 255, 255); font-size: 14px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">3. 참조자는 CSC 센터장, 인사담장자로 지정합니다. </span></p><p><span style=\"color: rgb(68, 68, 68); background-color: rgb(255, 255, 255); font-size: 14px; font-family: &quot;Open Sans&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">4. 검토자, 확인자는 지정하지 않습니다.</span></p><p><br></p>"
-
     return (
         <>
             <div className="container" style={{marginTop:"10px"}}>
@@ -53,7 +82,7 @@ const ElecAtrzNewReq = () => {
                         <Button text="목록" onClick={toAtrzNewReq}/>
                     </div>
                 </div>
-                <div style={{textAlign:"center"}}>선택한 기안문서의 명칭</div>
+                <div style={{textAlign:"center"}}>{formData.gnrlAtrzTtl}</div>
                 <div style={{display:"flex", justifyContent:"flex-start"}}>
                     <div style={{float: "left", marginRight:"auto"}}>
                         <table>
@@ -65,7 +94,7 @@ const ElecAtrzNewReq = () => {
                             <tr>
                                 <td>프로젝트</td>
                                 <td> : </td>
-                                <td>프로젝트명</td>
+                                <td>{prjctData.prjctNm}</td>
                             </tr>
                             <tr>
                                 <td>기안자</td>
@@ -80,10 +109,10 @@ const ElecAtrzNewReq = () => {
                         </table>
                     </div>
                     <div style={{display: "inline-block"}}>
-                    <table className="table-atrzLn">
+                        <table className="table-atrzLn">
                             <tbody>
                                 <tr>
-                                    <th className="table-atrzLn-th" rowspan={4}>결재</th>
+                                    <th className="table-atrzLn-th" rowSpan={4}>결재</th>
                                     <th className="table-atrzLn-th">검토</th>
                                     <th className="table-atrzLn-th">확인</th>
                                     <th className="table-atrzLn-th">심사</th>
@@ -108,38 +137,60 @@ const ElecAtrzNewReq = () => {
                                     <td className="table-atrzLn-td"></td>
                                 </tr>
                                 <tr>
-                                    <th className="table-atrzLn-th" rowspan={4}>합의</th>
-                                    <th className="table-atrzLn-td" colspan={4}></th>
+                                    <th className="table-atrzLn-th" rowSpan={4}>합의</th>
+                                    <th className="table-atrzLn-td" colSpan={4}></th>
                                 </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
                 <div style={{marginTop:"20px"}}>
-                    <table>
-                        <tr>
-                            <td>참조</td>
-                            <td> : </td>
-                        <td>
-                            CSC 김형균 전무;   경영지원팀 이진원 이사보;   경영지원팀 안주리 차장;   경영지원팀 안수민 차장;  부서 이름 직위;
-                        </td>
-                        </tr>
-                        <tr>
-                            <td>제목</td>
-                            <td> : </td>
-                            <td>텍스트 박스</td>
-                        </tr>
-                    </table>
+                    <div className="dx-fieldset">
+                        <div className="dx-field">
+                            <div className="dx-field-label" style={{width: "5%"}}>참 조</div>
+                            <TextBox
+                                className="dx-field-value"
+                                readOnly={true}
+                                style={{width: "95%"}}
+                                value="CSC 김형균 전무;   경영지원팀 이진원 이사보;   경영지원팀 안주리 차장;   경영지원팀 안수민 차장;  부서 이름 직위; 부서 이름 직위;"
+                            />
+                        </div>
+                        <div className="dx-field">
+                            <div className="dx-field-label" style={{width: "5%"}}>제 목</div>
+                            <TextBox
+                                className="dx-field-value"
+                                style={{width: "95%"}}
+                            />
+                        </div>
+                    </div>
                 </div>
                 <hr/>
-                <div dangerouslySetInnerHTML={{ __html: test }} />
-                {/* <HtmlEditBox 
-                /> */}
+                <div dangerouslySetInnerHTML={{ __html: formData.docFormDc }} />
+
+                {data.elctrnAtrzTySeCd === "VTW04908" && <ElecAtrzOutordEmpCtrt data={data} prjctId={prjctId}/>}
+
+
+                <HtmlEditBox 
+                    column={ {"dataField": "gnrlAtrzCn"}}
+                    data={data}
+                    setData={setData}
+                    value={data.gnrlAtrzCn}
+                    placeholder={column.placeholder}
+                />
+                <hr/>
+                <div style={{marginBottom: "30px"}}>
+                    <div> * 첨부파일</div>
+                    <FileUploader
+                        multiple={true}
+                        accept="*/*"
+                        uploadMode="useButton"
+                        // onValueChanged={handleAttachmentChange}
+                        maxFileSize={1.5 * 1024 * 1024 * 1024}
+                    />
+                </div>
             </div>
         </>
     );
-
-
 }
 
 export default ElecAtrzNewReq;
---
+++ front/src/pages/elecAtrz/ElecAtrzOutordEmpCtrt.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ElecAtrzOutordEmpCtrt = ({data, prjctId}) => {
+
+    console.log(data);
+    console.log(prjctId);
+
+    useEffect
+
+    
+
+
+
+    return (
+        <div>ElecAtrzOutordEmpCtrt</div>
+    );
+};
+export default ElecAtrzOutordEmpCtrt;
