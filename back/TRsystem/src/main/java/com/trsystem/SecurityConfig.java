3f2d7c28ca52a065ee99a7d58fae19789d4773aa
jin-kyu-kim <jinkyu.kim@vtw.co.kr>
2024-03-22 02:14:48 +0900

    로그인 오류 수정

--- back/TRsystem/src/main/java/com/trsystem/SecurityConfig.java
+++ back/TRsystem/src/main/java/com/trsystem/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable
-                        ).headers((headerConfig)->headerConfig.frameOptions((HeadersConfigurer.FrameOptionsConfig::disable)))
+                ).headers((headerConfig)->headerConfig.frameOptions((HeadersConfigurer.FrameOptionsConfig::disable)))
                 .authorizeHttpRequests((authorizeRequest)->
                         authorizeRequest
                                 .requestMatchers(allRequestMatcher()).permitAll()
