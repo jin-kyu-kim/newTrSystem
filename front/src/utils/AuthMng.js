50895795ed4002f073d0a3a061c6196368b9f524
ku1551 <jinkyu.kim@vtw.co.kr>
2024-03-21 21:40:06 +0900

    로그인 기능 수정

--- front/src/utils/AuthMng.js
+++ front/src/utils/AuthMng.js
@@ -1,13 +1,10 @@
 import ApiRequest from "./ApiRequest";
 import {Cookies} from "react-cookie";
 
-const defaultUser = null;
 export async function signIn(empno, password) {
     try {
         const param = {empno:empno, password:password};
-            console.log("1");
         const response = await ApiRequest("/boot/sysMng/lgnSkll", param);
-
             if(!response.fail){
                 return {
                     isOk: true,
@@ -57,8 +54,6 @@ export async function signOut() {
 export async function changePassword(empno, oldPassword, newPassword) {
     try {
         // Send request
-        console.log(empno, newPassword);
-
         return {
             isOk: true
         };
@@ -74,9 +69,7 @@ export async function changePassword(empno, oldPassword, newPassword) {
 export async function resetPassword() {
     try {
         const param = {empId:'2022f551-bf25-11ee-b259-000c2956283f',empno:'VK1545'};
-        console.log("1");
         const response = await ApiRequest("/boot/trs/sysMng/resetPswd", param);
-        console.log(response);
         return {
             isOk: true,
             data: response
