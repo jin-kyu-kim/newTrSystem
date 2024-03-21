e2b050e7701ac2f60ae1fd664fb468ccf2d554af
VtwHansolKim <157336316+VtwHansolKim@users.noreply.github.com>
2024-03-21 17:34:07 +0900

    전자결재 서식 에러 수정

--- front/src/pages/elecAtrz/ElecAtrzOutordEmpCtrt.jsx
+++ front/src/pages/elecAtrz/ElecAtrzOutordEmpCtrt.jsx
@@ -5,12 +5,6 @@ const ElecAtrzOutordEmpCtrt = ({data, prjctId}) => {
     console.log(data);
     console.log(prjctId);
 
-    useEffect
-
-    
-
-
-
     return (
         <div>ElecAtrzOutordEmpCtrt</div>
     );
