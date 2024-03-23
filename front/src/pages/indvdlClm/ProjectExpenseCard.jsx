5bc49144950624fbc711e2c19138da30769edca5
이승연 <seungyeon.lee@vtw.co.kr>
2024-03-23 17:53:41 +0900

    프로젝트비용 인서트 컴포넌트 분리

--- front/src/pages/indvdlClm/ProjectExpenseCard.jsx
+++ front/src/pages/indvdlClm/ProjectExpenseCard.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import CustomDateRangeBox from "../../components/unit/CustomDateRangeBox";
 import Box, {Item} from "devextreme-react/box";
 import {Button} from "devextreme-react/button";
@@ -10,6 +10,7 @@ import DataGrid, {
 import {TextBox} from "devextreme-react";
 import AutoCompleteProject from "../../components/unit/AutoCompleteProject";
 import {useCookies} from "react-cookie";
+import ProjectExpenseSubmit from "./ProjectExpenseSubmit";
 
 const button = {
     borderRadius: '5px',
@@ -24,6 +25,7 @@ const ProjectExpenseCard = (props) => {
     const [searchParam, setSearchParam] = useState();
     const [cardValue, setCardValue] = useState();
     const [cookies] = useCookies([]);
+    const [selectedItem, setSelectedItem] = useState();
 
     const handleStartDateChange = (value) => {
         setSearchParam({...searchParam, bgngYmd: value});
@@ -39,23 +41,47 @@ const ProjectExpenseCard = (props) => {
     };
 
     useEffect(() => {
+        let aplyDate = null;
+        let now = new Date();
+        let dateNum = Number(now.getDate());
+        if(dateNum <= 15){
+            let firstDayOfMonth = new Date( now.getFullYear(), now.getMonth() , 1 );
+            let lastMonth = new Date ( firstDayOfMonth.setDate( firstDayOfMonth.getDate() - 1 ) );
+            aplyDate = {
+                "aplyYm": lastMonth.getFullYear()+('0' + (lastMonth.getMonth()+1)).slice(-2),
+                "aplyOdr": 2
+            }
+        } else if (16 <= dateNum){
+            aplyDate = {
+                "aplyYm": now.getFullYear()+('0' + (now.getMonth()+1)).slice(-2),
+                "aplyOdr": 1
+            }
+        }
+
         const jsonValue = props.excel;
         const list = [];
         for(let i = 1; i < jsonValue?.length; i++){
+            const date = jsonValue[i].__EMPTY_4;
+            const time = jsonValue[i].__EMPTY_5;
             const data = {
-                "utztnDt" : "",
+                "utztnDt" : date.substring(2,4)+date.substring(5,7)+date.substring(8,10)+time.substring(0,2)+time.substring(3,5)+time.substring(6,8),
                 "useOffic" : jsonValue[i].__EMPTY_6,
-                "utztnAmt" : "",
-                "lotteCardSn" : "",
+                "utztnAmt" : jsonValue[i].__EMPTY_7,
+                "lotteCardSn" : jsonValue[i].__EMPTY_1,
                 "regEmpId" : cookies.userInfo.empId,
                 "empId" : cookies.userInfo.empId,
-                "aplyYm" : "",
-                "aplyOdr" : "",
-                "ctAtrzSeCd" : ""
+                "aplyYm" : aplyDate.aplyYm,
+                "aplyOdr" : aplyDate.aplyOdr,
+                "ctAtrzSeCd" : "VTW01903"
             };
             list.push(data);
         }
         setCardValue(list);
+    }, []);
+
+    const onSelectionChanged = useCallback((e) => {
+        const selectedRowsData = e.selectedRowsData;
+        setSelectedItem(selectedRowsData);
     }, []);
 
     const handleSubmit = () => {
@@ -130,7 +156,7 @@ const ProjectExpenseCard = (props) => {
             <div style={{marginBottom: 20}}>
                 <Button style={button} type='danger' text="선택한 사용내역 삭제하기"/>
                 <Button style={button} type='default' text="선택한 사용내역 전자결재 작성"/>
-                <Button style={button} text="선택한 사용내역 등록하기"/>
+                <ProjectExpenseSubmit text="선택한 사용내역 등록하기" value={selectedItem} tbNm="PRJCT_CT_APLY" snColumn="PRJCT_CT_APLY_SN"/>
             </div>
             <div style={fontSize}>
                 <p> ※ 일괄적용 버튼 클릭 시 체크박스로 선택한 항목 중 가장 위에서 선택한 항목으로 일괄적용 됩니다.<br/>
@@ -140,11 +166,11 @@ const ProjectExpenseCard = (props) => {
                     </span>
                 </p>
             </div>
-            <DataGrid dataSource={cardValue} showBorders={true} style={{width: "100%"}}>
+            <DataGrid dataSource={cardValue} onSelectionChanged={onSelectionChanged} showBorders={true} style={{width: "100%"}}>
                 <Selection mode="multiple" />
-                <Column dataField="utztnDt" caption="사용일시" width={120} />
-                <Column dataField="useOffic" caption="사용처" width={120}/>
-                <Column dataField="utztnAmt" caption="금액" width={120}/>
+                <Column dataField="utztnDt" caption="사용일시" minwidth={130} />
+                <Column dataField="useOffic" caption="사용처" minwidth={120}/>
+                <Column dataField="utztnAmt" caption="금액" minwidth={120}/>
                 <Column caption="프로젝트" cellRender={projectCell}/>
                 <Column caption="경비코드" cellRender={expanseCell}/>
                 <Column caption="용도" cellRender={purposeCell}/>
