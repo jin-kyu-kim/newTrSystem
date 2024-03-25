e14777e7ecc4e3f44addff2f8387ff9d7df20c7c
jun-hyeok-oh <junhyeok.oh@vtw.co.kr>
2024-03-25 21:13:26 +0900

    통합 경비 , 근무시간 쿼리 작성중 및 컨트롤러 추가

--- front/src/pages/fnnrMng/EmpTRCostTotal.js
+++ front/src/pages/fnnrMng/EmpTRCostTotal.js
@@ -6,15 +6,52 @@ import CustomTable from "components/unit/CustomTable";
 import SearchPrjctCostSet from "../../components/composite/SearchPrjctCostSet";
 
 const EmpTRCostTotal = () => {
-    const [values, setValues] = useState([]);
-    const [param, setParam] = useState({});
-  
-    const [totalItems, setTotalItems] = useState(0);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-    const [pageSize, setPageSize] = useState(20);
+  const [values, setValues] = useState([]);
+  const [param, setParam] = useState({});
+  const { keyColumn, queryId, tableColumns, searchParams  } = EmpTRCostTotalJson;
+  const [totalItems, setTotalItems] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [pageSize, setPageSize] = useState(20);
 
-    const { keyColumn, queryId, tableColumns, searchParams } = EmpTRCostTotalJson;
+
+
+  useEffect(() => {
+      pageHandle();
+  }, []);
+
+  // 검색으로 조회할 때
+  const searchHandle = async (initParam) => {
+    setTotalPages(1);
+    setCurrentPage(1);
+    setParam({
+      ...initParam,
+      queryId: queryId,
+      currentPage: currentPage,
+      startVal: 0,
+      pageSize: pageSize,
+    });
+  };
+
+  const pageHandle = async () => {
+    try {
+      const response = await ApiRequest("/boot/retrieveFnnrMngWorkHrCtUnityAprv", param);
+      setValues(response);
+      if (response.length !== 0) {
+        setValues(response);
+       console.log(values+"이게바로")
+      } else {
+        setTotalPages(1);
+        setTotalItems(0);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+
 
   return (
 
