b581b7b869408f66a8defd99f37e41849292999e
soo-yeon-yoon <sooyeon.yoon@vtw.co.kr>
2024-03-27 13:44:20 +0900

    프로젝트비용청구현황 개인별 수행인력 팝업 기본 날짜 수정

--- front/src/pages/fnnrMng/ProjectClaimCostDetail.js
+++ front/src/pages/fnnrMng/ProjectClaimCostDetail.js
@@ -17,41 +17,33 @@ const ProjectClaimCostDetail = () => {
     const searchInfo = ProjectClaimCostDetailJson.searchInfo;
     const [param, setParam] = useState([]);
 
-    let year = "";
-    let monthVal = "";
-    let aplyOdr = 0;
-    let empFlnm = "";
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
 
-    let startYmd = '';
-    let startOdr = '';
-    let endYmd = '';
-    let endOdr = '';
+    let odrVal = day > 15 ? "1" : "2";
+    let monthVal = month < 10 ? "0" + month : month;
+
+    const [startYmd, setStartYmd] = useState('');
+    const [startOdr, setStartOdr] = useState('');
+    const [endYmd, setEndYmd] = useState('');
+    const [endOdr, setEndOdr] = useState('');
+
+    let newStartYmd, newStartOdr, newEndYmd, newEndOdr;
 
     useEffect(() => {
 
-        const param = {
-            queryId: "financialAffairMngMapper.retrievePrjctCtClmSttusIndvdlMMAccto",
-            prjctId: prjctId,
-            startYmOdr: year+monthVal+aplyOdr,
-            endYmOdr: year+monthVal+aplyOdr,
-            empFlnm: empFlnm,
-        };
-
-        const response = ApiRequest("/boot/common/queryIdSearch", param);
+        setStartYmd(year + monthVal);
+        setStartOdr(odrVal);
+        setEndYmd(year + monthVal);
+        setEndOdr(odrVal);
 
     }, []);
 
     const searchHandle = async (initParam) => {
 
         if(initParam.startYmOdr == null && initParam.endYmOdr == null) {
-
-            const date = new Date();
-            const year = date.getFullYear();
-            const month = date.getMonth() + 1;
-            const day = date.getDate();
-
-            let odrVal = day > 15 ? "1" : "2";
-            let monthVal = month < 10 ? "0" + month : month;
 
             setParam({
                 ...param,
@@ -63,32 +55,37 @@ const ProjectClaimCostDetail = () => {
             return;
         } else if(initParam.startYmOdr !== null && initParam.endYmOdr == null) {
 
-            startYmd = initParam.startYmOdr.substr(0, 6);
-            startOdr = initParam.startYmOdr.substr(6, 2) > 15 ? "2" : "1";
-            endYmd = initParam.startYmOdr.substr(0, 6);
-            endOdr = initParam.startYmOdr.substr(6, 2) > 15 ? "2" : "1";
+            newStartYmd = initParam.startYmOdr.substr(0, 6);
+            newStartOdr = initParam.startYmOdr.substr(6, 2) > 15 ? "2" : "1";
+            newEndYmd = initParam.startYmOdr.substr(0, 6);
+            newEndOdr = initParam.startYmOdr.substr(6, 2) > 15 ? "2" : "1";
 
         } else if(initParam.startYmOdr == null && initParam.endYmOdr !== null) {
 
-            startYmd = initParam.endYmOdr.substr(0, 6);
-            startOdr = initParam.endYmOdr.substr(6, 2) > 15 ? "2" : "1";
-            endYmd = initParam.endYmOdr.substr(0, 6);
-            endOdr = initParam.endYmOdr.substr(6, 2) > 15 ? "2" : "1";
+            newStartYmd = initParam.endYmOdr.substr(0, 6);
+            newStartOdr = initParam.endYmOdr.substr(6, 2) > 15 ? "2" : "1";
+            newEndYmd = initParam.endYmOdr.substr(0, 6);
+            newEndOdr = initParam.endYmOdr.substr(6, 2) > 15 ? "2" : "1";
 
         } else if(initParam.startYmOdr !== null && initParam.endYmOdr !== null) {
 
-            startYmd = initParam.startYmOdr.substr(0, 6);
-            startOdr = initParam.startYmOdr.substr(6, 2) > 15 ? "2" : "1";
-            endYmd = initParam.endYmOdr.substr(0, 6);
-            endOdr = initParam.endYmOdr.substr(6, 2) > 15 ? "2" : "1";
+            newStartYmd = initParam.startYmOdr.substr(0, 6);
+            newStartOdr = initParam.startYmOdr.substr(6, 2) > 15 ? "2" : "1";
+            newEndYmd = initParam.endYmOdr.substr(0, 6);
+            newEndOdr = initParam.endYmOdr.substr(6, 2) > 15 ? "2" : "1";
 
         };
+
+        setStartYmd(newStartYmd);
+        setStartOdr(newStartOdr);
+        setEndYmd(newEndYmd);
+        setEndOdr(newEndOdr);
 
         setParam({
 
             ...param,
-            startYmOdr: startYmd+startOdr,
-            endYmOdr: endYmd+endOdr,
+            startYmOdr: newStartYmd+newStartOdr,
+            endYmOdr: newEndYmd+newEndOdr,
             empFlnm: initParam.empFlnm,
         })
 
@@ -106,21 +103,18 @@ const ProjectClaimCostDetail = () => {
 
     const itemTitleRender = (a) => <span>{a.TabName}</span>;
 
-
     return (
         <div >
             <div style={{ padding: "20px" }}>
                 <div className="col-md-10 mx-auto" style={{marginTop: "20px", marginBottom: "10px"}}>
                     <h1 style={{fontSize: "30px"}}>프로젝트비용청구현황</h1>
                     <span>* 선택하신 일자가 속한 차수 기준으로 검색됩니다.</span><br/>
-                    <span>* 일자를 하나만 선택하실 경우 해당 일자가 속한 하나의 차수만 조회됩니다.</span>
-                    {/*<span>* {prjctNm}({param.startYmOdr.substr(0, 6)}-{param.startYmOdr.substr(6, 1)}~{param.endYmOdr.substr(0, 6)}-{param.endYmOdr.substr(6, 1)})</span>*/}
+                    <span>* 일자를 하나만 선택하실 경우 해당 일자가 속한 하나의 차수만 조회됩니다.</span><br/>
+                    <span>* {prjctNm}({startYmd}-{startOdr}~{endYmd}-{endOdr})</span>
                 </div>
                 <div className="wrap_search" style={{marginBottom: "20px"}}>
                     <SearchInfoSet callBack={searchHandle} props={searchInfo}/>
                 </div>
-
-
                 <div
                     style={{
                         marginTop: "20px",
