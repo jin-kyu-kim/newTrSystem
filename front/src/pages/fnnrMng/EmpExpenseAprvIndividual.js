3576d6511a18d2f6d223e22d756ed48e6dca334e
soo-yeon-yoon <sooyeon.yoon@vtw.co.kr>
2024-03-27 19:13:31 +0900

    경비 승인내역 쿼리 수정

--- front/src/pages/fnnrMng/EmpExpenseAprvIndividual.js
+++ front/src/pages/fnnrMng/EmpExpenseAprvIndividual.js
@@ -6,12 +6,28 @@ import PivotGridDataSource from "devextreme/ui/pivot_grid/data_source";
 
 const EmpExpenseAprvIndividual = ({ empNo, aplyYm, aplyOdr, expensCd }) => {
 
-    const [expenseAprvData, setExpenstAprvData] = useState([]);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        getExpenseAprvData();
 
-    }, [aplyYm, aplyOdr]);
+        fetchData();
+
+    }, []);
+
+    //날짜 조회
+    const getDays = async () => {
+        const param = {
+            queryId: 'financialAffairMngMapper.retrieveDays',
+            aplyYm: aplyYm,
+            aplyOdr: aplyOdr,
+        };
+        try{
+            const response = await ApiRequest('/boot/common/queryIdSearch', param);
+            return response;
+        }catch (error){
+            console.log(error);
+        }
+    };
 
     //경비 승인내역 조회
     const getExpenseAprvData = async () => {
@@ -24,10 +40,52 @@ const EmpExpenseAprvIndividual = ({ empNo, aplyYm, aplyOdr, expensCd }) => {
         };
         try{
             const response = await ApiRequest('/boot/common/queryIdSearch', param);
-            setExpenstAprvData(response);
+            return response;
         }catch (error){
             console.log(error);
         }
+    };
+
+    const fetchData = async () => {
+        try {
+            // 두 쿼리를 실행하고 데이터를 조합하여 PivotGrid에 전달
+            const [columnData, rowData] = await Promise.all([getDays(), getExpenseAprvData()]);
+
+            // 데이터 조합
+            const combinedData = combineData(columnData, rowData);
+            setData(combinedData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const combineData = (columnData, rowData) => {
+        const combinedData = [];
+
+        const pivotDates = columnData.map(item => item.pivotDate);
+
+        for (const pivotDate of pivotDates) {
+            const matchingRowData = rowData.filter(item => item.utztnDt === pivotDate);
+
+            for (const row of matchingRowData) {
+                combinedData.push({
+                    pivotDate: pivotDate,
+                    empFlnm: row.empFlnm,
+                    prjctNm: row.prjctNm,
+                    expensCd: row.expensCd,
+                    empDetail: row.empDetail,
+                    utztnAmt: row.utztnAmt
+                });
+            }
+
+            if (matchingRowData.length === 0) {
+                combinedData.push({
+                    pivotDate: pivotDate,
+                });
+            }
+        }
+
+        return combinedData;
     };
 
     const dataSource = new PivotGridDataSource({
@@ -74,8 +132,11 @@ const EmpExpenseAprvIndividual = ({ empNo, aplyYm, aplyOdr, expensCd }) => {
             summaryType: 'sum',
             format: 'fixedPoint',
             area: 'data',
+            customizeText: function(cellInfo) {
+                return cellInfo.valueText === '0'  ? '' : cellInfo.valueText;
+            }
         }],
-        store: expenseAprvData,
+        store: data,
         retrieveFields: false,
     });
 
