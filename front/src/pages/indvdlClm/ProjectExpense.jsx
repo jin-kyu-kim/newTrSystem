827dc7cd752bd07c1f715a7fa865a6d4d66c2565
soo-yeon-yoon <sooyeon.yoon@vtw.co.kr>
2024-04-02 13:25:40 +0900

    프로젝트비용 청구 승인취소건 엑셀 업로드 제외

--- front/src/pages/indvdlClm/ProjectExpense.jsx
+++ front/src/pages/indvdlClm/ProjectExpense.jsx
@@ -1,44 +1,73 @@
-import React, {useCallback, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {TabPanel} from "devextreme-react";
 import ProjectExpenseJson from "./ProjectExpenseJson.json"
 import ProjectExpenseCell from "./ProjectExpenseCell";
+import {useCookies} from "react-cookie";
 
 const ProjectExpense = () => {
-  const ExpenseInfo = ProjectExpenseJson.ExpenseInfo;
-  const [index, setIndex] = useState(0);
-  const onSelectionChanged = useCallback(
-      (args) => {
-          if (args.name === "selectedIndex") {
-              setIndex(args.value);
-          }
-      },
-      [setIndex]
-  );
+    const ExpenseInfo = ProjectExpenseJson.ExpenseInfo;
+    const [index, setIndex] = useState(0);
+    const [aplyYm, setAplyYm] = useState();
+    const [aplyOdr, setAplyOdr] = useState();
+    const [cookies] = useCookies([]);
 
-  const itemTitleRender = (a) => <span>{a.TabName}</span>;
+    const empId = cookies.userInfo.empId;
 
-  return (
-      <div className="container">
-          <ProjectExpenseCell/>
-          <TabPanel
-              height="auto"
-              width="auto"
-              dataSource={ExpenseInfo}
-              selectedIndex={index}
-              onOptionChanged={onSelectionChanged}
-              itemTitleRender={itemTitleRender}
-              animationEnabled={true}
-              itemComponent={({ data }) => {
-                  const Component = React.lazy(() => import(`${data.url}`));
-                  return (
-                      <React.Suspense fallback={<div>Loading...</div>}>
-                          <Component index={index} setIndex={setIndex}/>
-                      </React.Suspense>
-                  );
-              }}
-          />
-      </div>
-  );
+    const onSelectionChanged = useCallback(
+        (args) => {
+            if (args.name === "selectedIndex") {
+                setIndex(args.value);
+            }
+        },
+        [setIndex]
+    );
+
+
+    useEffect(() => {
+
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+
+        let odrVal = day > 15 ? "2" : "1";
+        let monthVal = month < 10 ? "0" + month : month;
+
+        setAplyYm(year+monthVal);
+        setAplyOdr(odrVal);
+
+    }, []);
+
+    const itemTitleRender = (a) => <span>{a.TabName}</span>;
+
+    return (
+        <div className="container">
+            <ProjectExpenseCell/>
+            <TabPanel
+                height="auto"
+                width="auto"
+                dataSource={ExpenseInfo}
+                selectedIndex={index}
+                onOptionChanged={onSelectionChanged}
+                itemTitleRender={itemTitleRender}
+                animationEnabled={true}
+                itemComponent={({ data }) => {
+                    const Component = React.lazy(() => import(`${data.url}`));
+                    return (
+                        <React.Suspense fallback={<div>Loading...</div>}>
+                            <Component
+                                empId={empId}
+                                index={index}
+                                setIndex={setIndex}
+                                aplyYm={aplyYm}
+                                aplyOdr={aplyOdr}
+                            />
+                        </React.Suspense>
+                    );
+                }}
+            />
+        </div>
+    );
 };
 
 export default ProjectExpense;
--- front/src/pages/indvdlClm/ProjectExpenseExcel.jsx
+++ front/src/pages/indvdlClm/ProjectExpenseExcel.jsx
@@ -1,7 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ExcelUpload from "../../components/unit/ExcelUpload";
 import Button from "devextreme-react/button";
-import {useCookies} from "react-cookie";
 import ApiRequest from "../../utils/ApiRequest";
 
 const button = {
@@ -12,63 +11,69 @@ const button = {
 
 const ProjectExpenseExcel = (props) => {
 
-    const [cookies] = useCookies([]);
     const [excel, setExcel] = useState();
 
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    const hours = String(currentDate.getHours()).padStart(2, "0");
+    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+
+    const formattedDate = `${year}${month}${day}${hours}${minutes}${seconds}`;
+
     const onClick = async () => {
+        if (excel === undefined){
+            window.alert("파일을 등록해주세요.");
+            return;
+        }
 
-        const date = new Date();
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
+        let param = [];
 
-        let odrVal = day > 15 ? "2" : "1";
-        let monthVal = month < 10 ? "0" + month : month;
-
-        const param = [];
         param.push({
             tbNm: "CARD_USE_DTLS",
             snColumn: "CARD_USE_SN",
-            snSearch: {empId: cookies.userInfo.empId, aplyYm: year+monthVal, aplyOdr: odrVal}
-        })
+            snSearch: {
+                empId: props.empId,
+                aplyYm: props.aplyYm,
+                aplyOdr: props.aplyOdr
+            }
+        });
 
         if(excel[0].__EMPTY_4 === "승인일자" && excel[0].__EMPTY_5 === "승인시간") {
             for (let i = 1; i < excel?.length; i++) {
 
-                let utztnAmt;
+                if(!excel[i].__EMPTY_7.includes('-')){
 
-                if(excel[i].__EMPTY_7.includes('-')){
-                    utztnAmt = 0;
-                    console.log(i,utztnAmt)
-                }else {
-                    utztnAmt = excel[i].__EMPTY_7.replace(/,/g, "");
-                    console.log(i,utztnAmt)
+                    const utztnAmt = excel[i].__EMPTY_7.replace(/,/g, "");
+                    const date = excel[i].__EMPTY_4.replace(/\./g, "");
+                    const time = excel[i].__EMPTY_5.replace(/:/g, "");
+
+                    const data = {
+                        "empId": props.empId,
+                        "aplyYm": props.aplyYm,
+                        "aplyOdr": props.aplyOdr,
+                        "utztnDt": date+time,
+                        "useOffic": excel[i].__EMPTY_6,
+                        "utztnAmt": utztnAmt,
+                        "aprvNo": excel[i].__EMPTY_20,
+                        "prjctCtInptPsbltyYn": "Y",
+                        "regEmpId": props.empId,
+                        "regDt": formattedDate,
+                        "ctStlmSeCd": "VTW01903"
+                    };
+
+                    param.push(data);
                 }
-
-                const date = excel[i].__EMPTY_4;
-                const time = excel[i].__EMPTY_5;
-
-                const data = {
-                    "empId": cookies.userInfo.empId,
-                    "aplyYm": year+monthVal,
-                    "aplyOdr": odrVal,
-                    "utztnDt": date.substring(0, 4) + date.substring(5, 7) + date.substring(8, 10) + time.substring(0, 2) + time.substring(3, 5) + time.substring(6, 8),
-                    "useOffic": excel[i].__EMPTY_6,
-                    "utztnAmt": utztnAmt,
-                    "aprvNo": excel[i].__EMPTY_20,
-                    "prjctCtInptPsbltyYn": "Y",
-                    "regEmpId": cookies.userInfo.empId,
-                    "ctStlmSeCd": "VTW01903"
-                };
-                param.push(data);
             }
-
             try {
                 const response = await ApiRequest("/boot/common/commonInsert", param);
+
                 if (response === 1) {
-                        props.setIndex(1);
-                        window.alert("등록되었습니다.")
-                    }
+                    props.setIndex(1);
+                    window.alert("등록되었습니다.")
+                }
             } catch (error) {
                 window.alert("오류가 발생했습니다.");
                 console.error(error);
@@ -86,7 +91,6 @@ const ProjectExpenseExcel = (props) => {
                     <br/>
                     1. <a href="https://corp.lottecard.co.kr/app/LCMANAA_V100.lc" target="_blank">롯데법인카드 홈페이지</a> > 이용조회 > 승인내역조회에서 조회 후 저장한 엑셀파일을 업로드 합니다.<br/>
                     2. 엑셀 파일을 업로드 후 롯데법인카드 내역 청구 탭에서 선택하여 청구하시면 됩니다.<br/>
-                    ※ 승인번호가 같은 경우 기존 데이터를 수정합니다.
                 </span>
             </span>
             <ExcelUpload excel={excel} setExcel={setExcel}/>
