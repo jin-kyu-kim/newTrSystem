09dd1ad0c0f7ae1d58310c89aaf79be8e5626c9e
sera <sera.oh@vtw.co.kr>
2024-04-02 10:21:39 +0900

    공통컴포넌트 - 결재선 : 결재자 리스트 반환 추가

--- front/src/components/unit/ApprovalPopup.jsx
+++ front/src/components/unit/ApprovalPopup.jsx
@@ -4,18 +4,18 @@ import CustomEmpComboBox from './CustomEmpComboBox';
 import ApiRequest from 'utils/ApiRequest';
 import '../../assets/css/Style.css'
 
-const ApprovalPopupRe = ({ visible, atrzValue, onHiding }) => {
+const ApprovalPopup = ({ visible, atrzValue, onHiding }) => {
     const [aprvrEmpList, setAprvrEmpList] = useState(atrzValue);
     const [selectedEmp, setSelectedEmp] = useState({});
     const [stepCdList, setStepCdList] = useState([]);
     const tableTitle = ['입력', '결재단계', '결재권자'];
-
+    
     const getCloseButtonOptions = useCallback(
         () => ({
-            text: '닫기',
+            text: '등록',
             stylingMode: 'outlined',
-            type: 'danger',
-            onClick: onHiding,
+            type: 'default',
+            onClick: () => onHiding(aprvrEmpList),
         }),
         [onHiding]
     );
@@ -39,7 +39,7 @@ const ApprovalPopupRe = ({ visible, atrzValue, onHiding }) => {
     };
 
     const onAddEmp = (cdValue) => {
-        // 검토 / 확인 / 심사 / 승인에는 한명만 VTW00702~05
+        // 검토 / 확인 / 심사 / 승인에는 한명만
         const oneEmpCase = ['VTW00702', 'VTW00703', 'VTW00704', 'VTW00705'];
         const isOneCase = oneEmpCase.includes(cdValue);
         const hasEmpAlready = aprvrEmpList.some(emp => emp.approvalCode === cdValue);
@@ -47,12 +47,12 @@ const ApprovalPopupRe = ({ visible, atrzValue, onHiding }) => {
         
         if (!isEmpIdExist) {
             if (isOneCase && hasEmpAlready) {
-                alert('검토, 확인, 심사, 승인 단계 결재권자는 한명만 설정할 수 있습니다. 삭제 후 설정하시기 바랍니다.');
+                alert('검토, 확인, 심사, 승인 단계 결재권자는 한명만 설정할 수 있습니다. 다시 선택해주세요.');
             } else {
                 setAprvrEmpList([...aprvrEmpList, { ...selectedEmp, approvalCode: cdValue }]);
             }
         } else {
-            alert('이미 해당 결제단계에 등록되어 있습니다.');
+            alert('이미 해당 결재단계에 등록되어 있습니다.');
         }
     };
 
@@ -64,7 +64,7 @@ const ApprovalPopupRe = ({ visible, atrzValue, onHiding }) => {
     const addAprvrEmpArea = () => {
         return (
             <div>
-                <span style={{ color: 'blue' }}>조직도에서 결재자를 선택하거나 검색 후 입력 버튼을 이용해 결재권자를 설정합니다.</span>
+                <span style={{ color: 'blue' }}>결재자를 선택하거나 검색 후, 추가 버튼을 이용해 결재권자를 설정합니다.</span>
                 <CustomEmpComboBox
                     value={selectedEmp.empId}
                     readOnly={false}
@@ -83,41 +83,39 @@ const ApprovalPopupRe = ({ visible, atrzValue, onHiding }) => {
                         <div className="atrz-popup-cell atrz-popup-button" onClick={() => onAddEmp(cd.cdValue)}>추가</div>
                         <div className="atrz-popup-cell">{cd.cdNm}</div>
                         <div className="atrz-popup-cell">
-                            {aprvrEmpList.filter(emp => emp.approvalCode === cd.cdValue)
+                            {aprvrEmpList.length !== 0 && aprvrEmpList.filter(emp => emp.approvalCode === cd.cdValue)
                                 .map((emp, index) => (
                                     <div className='aprvrEmp' key={index}>
                                         {emp.empFlnm}
                                         <button onClick={() => removeEmp(emp.empId, emp.approvalCode)}
                                         className='popup-delete-btn'>X</button>
                                     </div>
-                                ))
-                            }
+                            ))}
                         </div>
                     </div>
                 ))}
             </div>
-        )
-    }
+        );
+    };
 
     return (
         <div>
             <Popup
-                height={700}
+                height={660}
                 visible={visible}
-                onHiding={onHiding}
+                onHiding={() => onHiding(aprvrEmpList)}
                 showCloseButton={true}
-                hideOnOutsideClick={true}
                 contentRender={addAprvrEmpArea}
                 title="* 결재선지정"
             >
                 <ToolbarItem
                     widget="dxButton"
                     toolbar="bottom"
-                    location="after"
+                    location="center"
                     options={getCloseButtonOptions()}
                 />
             </Popup>
         </div>
     );
 }
-export default ApprovalPopupRe;
+export default ApprovalPopup;
