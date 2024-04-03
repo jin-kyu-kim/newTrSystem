4e902eea1ced222fa184630d80f503930ccbe339
jihwan park <jihwan.park@vtw.co.kr>
2024-04-03 13:13:58 +0900

    2024_04_03_1313(박지환) 개인청구 소스 반영

--- back/TRsystem/src/main/java/com/trsystem/indvdlClm/controller/IndvdlClmController.java
+++ back/TRsystem/src/main/java/com/trsystem/indvdlClm/controller/IndvdlClmController.java
@@ -1,10 +1,16 @@
 package com.trsystem.indvdlClm.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trsystem.indvdlClm.domain.IndvdlClmDomain;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.MediaType;
 
 import java.util.List;
 import java.util.Map;
@@ -18,9 +24,28 @@ public class IndvdlClmController {
         return IndvdlClmDomain.insertPrjctMM(params);
     }
 
-    // 프로젝트근무시간 저장
-    @PostMapping(value = "/boot/indvdlClm/prjctMm/insertPrjctMmAply")
+    // 프로젝트근무시간저장
+    @PostMapping(value = "/boot/indvdlClm/insertPrjctMmAply")
     public List<Map<String, Object>> insertPrjctMmAply (@RequestBody List<Map<String, Object>> params){
         return IndvdlClmDomain.insertPrjctMmAply(params);
+    }
+
+    // 휴가결재저장
+    @PostMapping(value = "/boot/indvdlClm/insertVcatnAtrz", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public int insertVcatnAtrz (
+            @RequestPart(required = false) List<MultipartFile> attachments,
+            @RequestPart(required = false) String insertElctrnValue,
+            @RequestPart(required = false) String insertVcatnValue,
+            @RequestPart(required = false) String elctrnTbNm,
+            @RequestPart(required = false) String vcatnTbNm
+            ) throws JsonProcessingException {
+
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> elctrnTbMap = mapper.readValue(elctrnTbNm,Map.class);
+            Map<String, Object> insertElctrnMap = mapper.readValue(insertElctrnValue,Map.class);
+            Map<String, Object> vcatnTbMap = mapper.readValue(vcatnTbNm,Map.class);
+            Map<String, Object> insertVcatnMap = mapper.readValue(insertVcatnValue,Map.class);
+
+        return IndvdlClmDomain.insertVcatnAtrz(elctrnTbMap, insertElctrnMap, vcatnTbMap, insertVcatnMap, attachments);
     }
 }
