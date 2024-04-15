bba2eecaa22be410a2a5fea87b2b081df4fdc254
VtwHansolKim <157336316+VtwHansolKim@users.noreply.github.com>
2024-04-15 16:11:34 +0900

    청구결재 조회 시 세금계산서 컬럼 추가

--- front/src/pages/elecAtrz/ElecAtrzTabDetail.jsx
+++ front/src/pages/elecAtrz/ElecAtrzTabDetail.jsx
@@ -83,6 +83,7 @@ const ElecAtrzTabDetail = ({ dtlInfo, detailData }) => {
                 keyColumn={"rowId"}
                 groupingData={groupingData}
                 groupingCustomizeText={groupingCustomizeText}
+                wordWrap={true}
             />
             </div>
         );
