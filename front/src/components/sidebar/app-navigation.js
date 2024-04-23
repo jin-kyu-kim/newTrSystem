4c1491de98d8c691e31889330fe60f0e15a9210d
honggukim2 <honggu.kim@vtw.co.kr>
2024-04-23 18:18:13 +0900

    20240423 1818 김홍구 수정사항 커밋

--- front/src/components/sidebar/app-navigation.js
+++ front/src/components/sidebar/app-navigation.js
@@ -68,10 +68,10 @@ export const navigation = [
         path: '/project/ProjectOutordAprv'
       },{
         text: '파트너업체관리',
-        path: 'project/ProjectOutordCompany'
+        path: '/project/ProjectOutordCompany'
       },{
         text: '파트너직원관리',
-        path: 'project/ProjectOutordEmp'
+        path: '/project/ProjectOutordEmp'
       },
     ]
   }, {
