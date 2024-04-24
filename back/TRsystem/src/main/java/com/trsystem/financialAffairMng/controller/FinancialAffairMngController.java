849cf42b8cd9cc071ddaaffd167381bc90a743fb
VtwHansolKim <157336316+VtwHansolKim@users.noreply.github.com>
2024-04-24 17:47:16 +0900

    근무시간비용입력 현황 조회 로직 수정

---
+++ back/TRsystem/src/main/java/com/trsystem/financialAffairMng/controller/FinancialAffairMngController.java
@@ -0,0 +1,23 @@
+package com.trsystem.financialAffairMng.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trsystem.financialAffairMng.domain.FinancialAffairMngDomain;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class FinancialAffairMngController {
+
+	@PostMapping(value = "/boot/financialAffairMng/retrieveCtData")
+	public List<Map<String, Object>> retrieveCtData(@RequestBody Map<String, Object> params) {
+		
+		return FinancialAffairMngDomain.retrieveCtData(params);
+	}
+}
