d65975af999fd412be97cb86eb2fda3612d51337
honggukim2 <honggu.kim@vtw.co.kr>
2024-05-03 14:17:20 +0900

    202405031417 김홍구 수정사항 커밋

--- front/src/components/sidebar/api/auth.js
+++ front/src/components/sidebar/api/auth.js
@@ -5,9 +5,8 @@ import ApiRequest from "../../../utils/ApiRequest";
 export async function signIn(empno, password) {
   try {
     const param = {empno:empno, password:password};
-    const token = localStorage.getItem("token");
     const response = await axios.post("/boot/sysMng/lgnSkll", param, {
-        headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}` },
+        headers: { "Content-Type": "application/json", },
       });
       if(response){
       localStorage.setItem("isLoggedIn", true);
