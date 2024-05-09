6e80c42312ad0a505daaa95b70c0008b50591cff
VtwHansolKim <157336316+VtwHansolKim@users.noreply.github.com>
2024-05-09 09:34:59 +0900

    근무시간비용 입력현황 세부 조회 쿼리 수정 및 시간/비용 취소 위한 쿼리 작성

---
+++ front/src/pages/fnnrMng/TimeExpenseCancelPopup.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import CustomEditTable from 'components/unit/CustomEditTable';
+
+const TimeExpenseCancelPopup = () => {
+
+}
+export default TimeExpenseCancelPopup;
