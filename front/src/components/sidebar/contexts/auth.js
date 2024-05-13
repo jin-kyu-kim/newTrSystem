5d718db85fe39c63e9336773d0b991bed57b7d0d
ku1551 <jinkyu.kim@vtw.co.kr>
2024-05-14 08:34:15 +0900

    localStorage 변경

--- front/src/components/sidebar/contexts/auth.js
+++ front/src/components/sidebar/contexts/auth.js
@@ -29,6 +29,9 @@ function AuthProvider(props) {
       setCookie("userAuth", result.data.data.authorities);
       setCookie("userInfo", result.data.data.userInfo);
       setCookie("deptInfo", result.data.data.deptInfo);
+      localStorage.setItem("userAuth", result.data.data.authorities);
+      localStorage.setItem("userInfo", result.data.data.userInfo);
+      localStorage.setItem("deptInfo", result.data.data.deptInfo);
       if(result.data.data.userInfo.intlPwsdYn && result.data.data.userInfo.intlPwsdYn === 'Y'){
         await setIntlPwsdYn(result.data.data.userInfo.empId, 'N');
         navigate("/infoInq/empDetailInfo");
