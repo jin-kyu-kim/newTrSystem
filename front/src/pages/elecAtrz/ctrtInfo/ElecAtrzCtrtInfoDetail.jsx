68001871fd23e2dd28c9ed6e787f8dda8aa28f41
seulgi <seulgi.heo@vtw.co.kr>
2024-05-22 13:11:20 +0900

    "VTW00801","VTW00802" 코드 추가

--- front/src/pages/elecAtrz/ctrtInfo/ElecAtrzCtrtInfoDetail.jsx
+++ front/src/pages/elecAtrz/ctrtInfo/ElecAtrzCtrtInfoDetail.jsx
@@ -31,7 +31,7 @@ const ElecAtrzCtrtInfoDetail = ({data, prjctId, onSendData, sttsCd, ctrtTyCd}) =
     /*
     *상태코드에 따른 버튼 변경
     */
-    if(["VTW03702","VTW03703","VTW03704","VTW03705","VTW03706","VTW03707","VTW03405"].includes(sttsCd) // 입력이 불가능한 상태로 만드는 것. 취소결재 만들때는 필요할듯
+    if(["VTW03702","VTW03703","VTW03704","VTW03705","VTW03706","VTW03707","VTW03405","VTW00801","VTW00802"].includes(sttsCd) // 입력이 불가능한 상태로 만드는 것. 취소결재 만들때는 필요할듯
         || ["VTW04911","VTW04912","VTW04913","VTW04914",].includes(data.elctrnAtrzTySeCd)){
         tableColumns = tableColumns.filter(item => item.value !== '삭제');
 
@@ -58,7 +58,7 @@ const ElecAtrzCtrtInfoDetail = ({data, prjctId, onSendData, sttsCd, ctrtTyCd}) =
         if(sttsCd === "VTW03701") {
             getTempData();
         /* 전자결재 목록 조회 */
-        }else if(["VTW03702","VTW03703","VTW03704","VTW03705"].includes(sttsCd)) {
+        }else if(["VTW03702","VTW03703","VTW03704","VTW03705","VTW00801","VTW00802"].includes(sttsCd)) {
             getTempData();
         }else if(["VTW03405"].includes(sttsCd)){   //지급
             getTempData();
@@ -236,7 +236,7 @@ const ElecAtrzCtrtInfoDetail = ({data, prjctId, onSendData, sttsCd, ctrtTyCd}) =
     return (
         <div className="elecAtrzNewReq-ctrtInfo">
             <div style={{ textAlign: "right", marginBottom:"10px" }}>
-                {(!["VTW03702","VTW03703","VTW03704","VTW03705","VTW03706","VTW03707","VTW03405"].includes(sttsCd)) && (
+                {(!["VTW03702","VTW03703","VTW03704","VTW03705","VTW03706","VTW03707","VTW03405","VTW00801","VTW00802"].includes(sttsCd)) && (
                     !["VTW04911","VTW04912","VTW04913","VTW04914",].includes(data.elctrnAtrzTySeCd) // 취소 시 수정
                 ) && (
                 <Button name="insert" onClick={()=>handlePopupVisible({name:"insert"})}>{insertButton}</Button>
