e25c4f0355fbe806ce8930ce3225e392f5b94f52
jin-kyu-kim <jinkyu.kim@vtw.co.kr>
2024-07-12 10:54:09 +0900

    문체비 마감목록 엑셀

--- front/src/pages/fnnrMng/TimeExpenseClosingList.jsx
+++ front/src/pages/fnnrMng/TimeExpenseClosingList.jsx
@@ -90,19 +90,91 @@ const TimeExpenseClosingList = () => {
     };
     const currentDateTime = new Date();
     const formattedDateTime = `${currentDateTime.getFullYear()}_` + `${padNumber(currentDateTime.getMonth() + 1)}_` + `${padNumber(currentDateTime.getDate())}`
-
     const onExporting = (e) => {
         const workbook = new Workbook();
         const worksheet = workbook.addWorksheet('Main sheet');
+
         exportDataGrid({
             component: e.component,
             worksheet,
             autoFilterEnabled: true,
+            customizeCell: ({ gridCell, excelCell }) => {
+                // 모든 셀의 테두리를 검은색으로 지정
+                excelCell.border = {
+                    top: { style: 'thin', color: { argb: 'FF000000' } },
+                    left: { style: 'thin', color: { argb: 'FF000000' } },
+                    bottom: { style: 'thin', color: { argb: 'FF000000' } },
+                    right: { style: 'thin', color: { argb: 'FF000000' } }
+                };
+
+                // 헤더를 제거
+                if (gridCell.rowType === 'header') {
+                    gridCell.skip = true;
+                }
+            },
         }).then(() => {
+            worksheet.spliceRows(1, 1);
+
+            worksheet.eachRow((row, rowIndex) => {
+                row.getCell('G').value = row.getCell('E').value;
+                row.getCell('E').value = null;
+                row.getCell('F').value = null;
+                row.getCell('G').border = {
+                    top: { style: 'thin', color: { argb: 'FF000000' } },
+                    left: { style: 'thin', color: { argb: 'FF000000' } },
+                    bottom: { style: 'thin', color: { argb: 'FF000000' } },
+                    right: { style: 'thin', color: { argb: 'FF000000' } }
+                };
+
+                // 원래 E와 F 셀에도 테두리 추가
+                row.getCell('E').border = {
+                    top: { style: 'thin', color: { argb: 'FF000000' } },
+                    left: { style: 'thin', color: { argb: 'FF000000' } },
+                    bottom: { style: 'thin', color: { argb: 'FF000000' } },
+                    right: { style: 'thin', color: { argb: 'FF000000' } }
+                };
+
+                row.getCell('F').border = {
+                    top: { style: 'thin', color: { argb: 'FF000000' } },
+                    left: { style: 'thin', color: { argb: 'FF000000' } },
+                    bottom: { style: 'thin', color: { argb: 'FF000000' } },
+                    right: { style: 'thin', color: { argb: 'FF000000' } }
+                };
+            });
+
+            // 각 열의 최대 너비를 계산하여 설정
+            worksheet.columns.forEach(column => {
+                let maxLength = 10; // 최소 너비 설정
+                column.eachCell({ includeEmpty: true }, cell => {
+                    const columnLength = cell.value ? cell.value.toString().length : 10;
+                    if (columnLength > maxLength) {
+                        maxLength = columnLength;
+                    }
+                });
+                column.width = maxLength + 2; // 약간의 여백 추가
+            });
+
+            worksheet.pageSetup = {
+                paperSize: 9, // A4
+                fitToWidth: 1,
+                fitToHeight: 0,
+                margins: {
+                    left: 0.25,   // 왼쪽 여백
+                    right: 0.25,  // 오른쪽 여백
+                    top: 0.25,    // 위쪽 여백
+                    bottom: 0.25, // 아래쪽 여백
+                    header: 0.1,  // 헤더 여백
+                    footer: 0.1   // 풋터 여백
+                },
+                scale: 75,
+            };
+
             workbook.xlsx.writeBuffer().then((buffer) => {
-            saveAs(new Blob([buffer], { type: 'application/octet-stream' }), '근무시간 경비 통합승인내역'+formattedDateTime+'.xlsx');
+                saveAs(new Blob([buffer], { type: 'application/octet-stream' }), '근무시간 경비 통합승인내역' + props.aplyYm+'-'+props.aplyOdr + '.xlsx');
             });
         });
+
+        e.cancel = true; // 기본 동작 취소
     };
 
 
